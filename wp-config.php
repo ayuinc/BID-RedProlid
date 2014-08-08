eebeee462cc147e9629443d8db28acea400cd240
julio <jcmoron82@gmail.com>
2014-08-08 16:57:19 -0500

    instalas disqus

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'prolid');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'redprolid2014');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
