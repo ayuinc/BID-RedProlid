2456db4326d56b72e449a6bc3b3d2529fed4dbf0
Herman Marin <herman.marin@ayuinc.com>
2014-09-08 11:10:09 -0500

    Conectando el CMS

---
+++ wp-content/themes/redprolid/include-header.php
@@ -0,0 +1,40 @@
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="logo">
+              <a href="<?php echo home_url(); ?>"><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/logo_redprolid-header.png" class="img-responsive"></a>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-3">
+            <div class="user-sign-up text-center">
+              <h3>¿Quieres ser parte <br>de la Red PROLID?</h3>
+              <a href="#" class="btn btn-primary">Únete</a>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-4">
+            <div class="user-sign-in-form">
+              <form>
+                <h4>Mi información</h4>
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Usuario o correo electrónico">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="Contraseña">
+                </div>
+                <div class="form-group row">
+                  <div class="col-xs-7">
+                    <a href="#" class="">Olvidé mi contraseña</a>
+                  </div>
+                  <div class="col-xs-5 text-right">
+                    <button type="submit" class="btn btn-primary btn-sm">Ingresa</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
---
+++ wp-content/themes/redprolid/page-dtl-seccion.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Template Name: DTL-Seccion
+*/
+
+get_header(); ?>
+
+    <!--HEADER-->
+    <?php get_template_part( 'include', 'header' ); ?>
+    
+    <!--NAV-->
+    <?php get_template_part( 'include', 'nav' ); ?>
+    <div class="mh-700">
+      <section id="dtl-adonde-vamos">
+        <div class="container relative dtl-content">
+         <!-- Start the Loop. -->
+					<?php if ( have_posts() ) : ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+		          <div class="absolute dtl-custom-heading">
+		            <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-adonde-vamos-icon.png" alt="<?php the_title(); ?>"></div>
+		            <h1 style="color: #007c8d;"><?php the_title(); ?></h1>
+		            <hr style="border-top: 3px dotted #007c8d;">
+		          </div>
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">
+		          <p class="text-left pt-14-100"><?php the_field('contenido-dtl-seccion'); ?></p>
+						<?php endwhile; ?>
+					<?php endif; ?>        
+        </div>
+      </section>
+      <section class="sumate-al-debate">
+        <div class="container">
+          <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate.png" alt="" class="ribbon">
+          <div class="row">
+            <div class="col-sm-9 pt-35">
+						<?php 
+						    $uri = $_SERVER['REQUEST_URI'];
+						    $elements = explode('/', $uri) ;
+						    $segment1 = $elements[1] ; // For the first segment
+						    $segment2 = $elements[2] ; //For the 2nd segment
+						    $segment3 = $elements[3] ; //For the 3rd segment
+						?>         
+            <?php query_posts( 'category_name='.$segment2.'&posts_per_page=1' ); ?>	
+							<?php while ( have_posts() ) : the_post(); ?>
+	              <div class="title">
+	                <h3><?php the_title(); ?></h3>
+	                <p>FUENTE: <?php the_field('fuente-debates'); ?></p>
+	                <span>Blog: <?php the_field('blog-debates'); ?></span>
+	              </div>
+	              <div class="content">
+	                <?php the_field('contenido-debates'); ?>
+	              </div>
+	              <div class="footer">
+	                <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
+	              </div>
+							<?php endwhile; ?>                
+            </div>
+            <div class="col-sm-3">
+              <div class="debates-anteriores">
+                <h4>¿Quieres ver los debates anteriores?</h4>
+                <div class="text-right">
+                  <a href="#" class="btn btn-primary">Ir a archivos</a>
+                </div>
+              </div>
+              <h4>Lo último en las redes</h4>
+            </div>
+          </div>
+        </div>
+      </section>
+    <!--NAVIMAGEN-->
+    <?php get_template_part( 'include', 'navimage' ); ?>    
+    </div>
+
+<?php get_footer(); ?>    
