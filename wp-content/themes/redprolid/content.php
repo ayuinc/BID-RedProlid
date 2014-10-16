ccb471b88c81235b02a33ef3c29dbe32557b6dd5
carlos calderon <cadermino@gmail.com>
2014-10-16 18:03:15 -0500

    single template

--- wp-content/themes/redprolid/content.php
+++ wp-content/themes/redprolid/content.php
@@ -4,70 +4,40 @@
  *
  * Used for both single and index/archive/search.
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
  */
 ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-		<div class="featured-post">
-			<?php _e( 'Featured post', 'twentytwelve' ); ?>
-		</div>
-		<?php endif; ?>
-		<header class="entry-header">
-			<?php if ( ! post_password_required() && ! is_attachment() ) :
-				the_post_thumbnail();
-			endif; ?>
+<!-- <div class="container grid-block-lg">
 
-			<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php else : ?>
-			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-			</h1>
-			<?php endif; // is_single() ?>
-			<?php if ( comments_open() ) : ?>
-				<div class="comments-link">
-					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-				</div><!-- .comments-link -->
-			<?php endif; // comments_open() ?>
-		</header><!-- .entry-header -->
+  <?php //query_posts( 'category_name=puntos-de-vista' ); ?> 
+  <?php //while ( have_posts() ) : the_post(); ?>
+  <div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10 with-hr">
+      <h3><?php //the_field('nombre_completo'); ?> <small>(Bolivia)</small></h3>
+      <p><?php //the_field('profesion'); ?> / <a href="#"><?php //the_field('cuenta_de_twitter'); ?></a></p>
+      <p class="lh-lg text-justify"><?php the_title(); ?></p>
+      <p class="lh-lg text-justify"><?php //the_field('contenido_punto_de_vista'); ?></p>
+      <p class="text-gray">Diciembre 2013</p>
+      <div class="text-right">
+        <a href="#">Lee el artículo >></a>
+      </div>
+    </div>
+    <div class="col-sm-1"></div>
+  </div>
+  <?php //endwhile; ?>
 
-		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		<?php else : ?>
-		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<?php endif; ?>
+  <div class="text-center">
+    <ul class="pager">
+      <li><a href="#">Previos</a></li>
+      <li><a href="#">Siguiente</a></li>
+    </ul>
+  </div>
+</div> -->
 
-		<footer class="entry-meta">
-			<?php twentytwelve_entry_meta(); ?>
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
-				<div class="author-info">
-					<div class="author-avatar">
-						<?php
-						/** This filter is documented in author.php */
-						$author_bio_avatar_size = apply_filters( 'twentytwelve_author_bio_avatar_size', 68 );
-						echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
-						?>
-					</div><!-- .author-avatar -->
-					<div class="author-description">
-						<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
-						<p><?php the_author_meta( 'description' ); ?></p>
-						<div class="author-link">
-							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
-							</a>
-						</div><!-- .author-link	-->
-					</div><!-- .author-description -->
-				</div><!-- .author-info -->
-			<?php endif; ?>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post -->
+<div class="container grid-block-lg">
+	<?php the_content(); ?>
+</div>
+<div class="container grid-block-lg">
+	<?php get_post_meta(); ?>
+</div>	
--- wp-content/themes/redprolid/single.php
+++ wp-content/themes/redprolid/single.php
@@ -2,32 +2,68 @@
 /**
  * The Template for displaying all single posts
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
  */
 
 get_header(); ?>
-
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<div class="test"><?php get_template_part( 'content', get_post_format() ); ?></div>
-
-				<nav class="nav-single">
-					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
-				</nav><!-- .nav-single -->
-
-				<?php comments_template( '', true ); ?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
+<!--HEADER-->
+<?php get_template_part( 'include', 'header' ); ?>
+<!--NAV-->
+<?php get_template_part( 'include', 'nav' ); ?>
+<?php query_posts( 'category_name=puntos-de-vista' ); ?> 
+<div class="mh-700">
+  <section id="puntos-de-vista">
+    <div class="container relative with-shadow mb-28">
+      <div class="absolute dtl-custom-heading">
+        <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-icon.png" alt=""></div>
+        <div class="clearfix">
+          <div class="col-sm-6">
+            <h1 style="color: #aeab8b;">Puntos de vista</h1>
+          </div>
+          <div class="col-sm-6 pt-28">
+            <nav class="text-right">
+              <a href="#">Puntos de vista anteriores</a> | <a href="#" data-toggle="modal" data-target="#myModal">¿Te gustaría proponernos un tema?</a>
+            </nav>
+          </div>
+        </div>
+        <hr style="border-top: 3px dotted #aeab8b;">
+      </div>
+      <h4 class="pt-14-100 light">Te ofrecemos artículos escritos por mujeres líderes en la región latinoamericana: políticas, especialistas o profesionales relevantes que ponen sobre el papel su experiencia en pro de unas sociedades más equitativas.</h4>
+    </div>
+    <div class="container text-justify with-hr">
+      <div class="row">
+        <div class="col-sm-3">
+          <img src="<?php the_field('imagen_punto_de_vista'); ?>" alt="" class="img-responsive">
+        </div>
+        <div class="col-sm-9">
+          <h3><?php the_field('nombre_completo'); ?> <small>(Bolivia)</small></h3>
+          <p><?php the_field('profesion'); ?> / <a href="http://twitter.com/<?php the_field('cuenta_de_twitter'); ?>">@<?php the_field('cuenta_de_twitter'); ?></a></p>
+          <p class="lh-lg text-justify">
+          	<?php the_field('descripcion_punto_de_vista'); ?>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="container text-justify with-hr">
+  	  <h4><?php the_title(); ?></h4>
+      <p><?php the_field('contenido_punto_de_vista'); ?></p>
+      <div class="text-center">
+        <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
+      </div>          
+    </div>
+    <div class="container with-shadow mh-350">
+      
+    </div>
+    <div class="container with-hr">
+      <h4>Artículos comentados</h4>
+      <ul class="grid-list grid-list-2 pb-ch-7">
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur!</a>/ Loreto Sating</li>
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur!</a>/ Loreto Sating</li>
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur!</a>/ Loreto Sating</li>
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur!</a>/ Loreto Sating</li>
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur!</a>/ Loreto Sating</li>
+      </ul>
+    </div>
+  </section>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
