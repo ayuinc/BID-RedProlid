6d4f6b1094194a1abdd0f4fda3404767bcecb626
carlos calderon <cadermino@gmail.com>
2014-10-17 19:04:03 -0500

    poll

--- wp-content/themes/redprolid/page-poll.php
+++ wp-content/themes/redprolid/page-poll.php
@@ -4,6 +4,6 @@ Template Name: Poll page
 */
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-	<?php //the_content(); ?>
+	<?php the_content(); ?>
 <?php endwhile; ?>
 <?php get_footer(); ?>
