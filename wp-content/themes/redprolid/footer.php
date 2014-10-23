e932a81247901e396ea67af6b21a2fcc212b28c9
carlos calderon <cadermino@gmail.com>
2014-10-23 17:28:03 -0500

    mailchimp

--- wp-content/themes/redprolid/footer.php
+++ wp-content/themes/redprolid/footer.php
@@ -156,22 +156,6 @@
     <div class="modal fade" id="mailChimp" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-            <h4 class="modal-title" id="myModalLabel">Recupera tu contraseña</h4>
-          </div>
-          <form>
-            <div class="modal-body">
-                <p>Envíaremos tu contraseña al correo electrónico que escribas a continuación, este debe ser el mismo que incluiste al registrarte en Red PROLID.</p>
-                <div class="form-group">
-                  <label for="email">Correo Electrónico:</label>
-                  <input type="email" class="form-control">
-                </div>
-            </div>
-            <div class="modal-footer">
-              <button type="submit" class="btn btn-primary">Enviar</button>
-            </div>
-          </form>
           <?php mailchimpSF_signup_form(); ?>
         </div>
       </div>
