96bce9115af12afcb311b045036ba3918a6c6028
carlos calderon <cadermino@gmail.com>
2014-10-23 17:25:36 -0500

    mailchimp

--- wp-content/plugins/mailchimp/mailchimp_widget.php
+++ wp-content/plugins/mailchimp/mailchimp_widget.php
@@ -241,8 +241,8 @@ function mailchimpSF_signup_form($args = array()) {
 		?>
 		</div>
 		<div class="modal-footer">
-		<div class="mc_signup_submit">
-			<input type="submit" name="mc_signup_submit" id="mc_signup_submit" value="<?php echo esc_attr($submit_text); ?>" class="button" />
+		<div class="btn btn-primary">
+			<input style="width:inherit" type="submit" name="mc_signup_submit" id="mc_signup_submit" value="<?php echo esc_attr($submit_text); ?>" class="button" />
 		</div><!-- /mc_signup_submit -->
 		</div>
 	
