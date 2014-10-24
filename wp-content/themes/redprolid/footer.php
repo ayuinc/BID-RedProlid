4d9ab0dd89df22fa34ba774b6a9d6295646db208
carlos calderon <cadermino@gmail.com>
2014-10-24 15:29:55 -0500

    reset password

--- wp-content/themes/redprolid/footer.php
+++ wp-content/themes/redprolid/footer.php
@@ -76,68 +76,69 @@
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
             <h4 class="modal-title" id="myModalLabel">Recupera tu contraseña</h4>
           </div>
-          <?php  
-  
-            global $wpdb;
-            
-            $error = '';
-            $success = '';
-            
-            // check if we're in reset form
-            if( isset( $_POST['action'] ) && 'reset' == $_POST['action'] ) 
-            {
-              $email = trim($_POST['user_login']);
-              
-              if( empty( $email ) ) {
-                $error = 'Enter a username or e-mail address..';
-              } else if( ! is_email( $email )) {
-                $error = 'Invalid username or e-mail address.';
-              } else if( ! email_exists( $email ) ) {
-                $error = 'There is no user registered with that email address.';
-              } else {
-                
-                $random_password = wp_generate_password( 12, false );
-                $user = get_user_by( 'email', $email );
-                
-                $update_user = wp_update_user( array (
-                    'ID' => $user->ID, 
-                    'user_pass' => $random_password
-                  )
-                );
-                
-                // if  update user return true then lets send user an email containing the new password
-                if( $update_user ) {
-                  $to = $email;
-                  $subject = 'Your new password';
-                  $sender = get_option('name');
-                  
-                  $message = 'Your new password is: '.$random_password;
-                  
-                  $headers[] = 'MIME-Version: 1.0' . "\r\n";
-                  $headers[] = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-                  $headers[] = "X-Mailer: PHP \r\n";
-                  $headers[] = 'From: '.$sender.' < '.$email.'>' . "\r\n";
-                  
-                  $mail = wp_mail( $to, $subject, $message, $headers );
-                  if( $mail )
-                    $success = 'Check your email address for you new password.';
-                    
-                } else {
-                  $error = 'Oops something went wrong updaing your account.';
-                }
-                
-              }
-              
-              if( ! empty( $error ) )
-                echo '<div class="message"><p class="error"><strong>ERROR:</strong> '. $error .'</p></div>';
-              
-              if( ! empty( $success ) )
-                echo '<div class="error_login"><p class="success">'. $success .'</p></div>';
-            }
-          ?>
+          
           <!--html code-->
           <form method="post">
             <div class="modal-body">
+              <?php  
+  
+                global $wpdb;
+                
+                $error = '';
+                $success = '';
+                
+                // check if we're in reset form
+                if( isset( $_POST['action'] ) && 'reset' == $_POST['action'] ) 
+                {
+                  $email = trim($_POST['user_login']);
+                  
+                  if( empty( $email ) ) {
+                    $error = 'Enter a username or e-mail address..';
+                  } else if( ! is_email( $email )) {
+                    $error = 'Invalid username or e-mail address.';
+                  } else if( ! email_exists( $email ) ) {
+                    $error = 'There is no user registered with that email address.';
+                  } else {
+                    
+                    $random_password = wp_generate_password( 12, false );
+                    $user = get_user_by( 'email', $email );
+                    
+                    $update_user = wp_update_user( array (
+                        'ID' => $user->ID, 
+                        'user_pass' => $random_password
+                      )
+                    );
+                    
+                    // if  update user return true then lets send user an email containing the new password
+                    if( $update_user ) {
+                      $to = $email;
+                      $subject = 'Your new password';
+                      $sender = get_option('name');
+                      
+                      $message = 'Your new password is: '.$random_password;
+                      
+                      $headers[] = 'MIME-Version: 1.0' . "\r\n";
+                      $headers[] = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                      $headers[] = "X-Mailer: PHP \r\n";
+                      $headers[] = 'From: '.$sender.' < '.$email.'>' . "\r\n";
+                      
+                      $mail = wp_mail( $to, $subject, $message, $headers );
+                      if( $mail )
+                        $success = 'Check your email address for you new password.';
+                        
+                    } else {
+                      $error = 'Oops something went wrong updaing your account.';
+                    }
+                    
+                  }
+                  
+                  if( ! empty( $error ) )
+                    echo '<div class="message"><p class="error"><strong>ERROR:</strong> '. $error .'</p></div>';
+                  
+                  if( ! empty( $success ) )
+                    echo '<div class="error_login"><p class="success">'. $success .'</p></div>';
+                }
+              ?>
               <p>Envíaremos tu contraseña al correo electrónico que escribas a continuación, este debe ser el mismo que incluiste al registrarte en Red PROLID.</p>
               <div class="form-group">
                 <label for="user_login">Correo Electrónico:</label>
