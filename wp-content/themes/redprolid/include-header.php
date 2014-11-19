4253e3317baa032c7e7bd75c6ad409ec16ffc82b
carlos calderon <cadermino@gmail.com>
2014-11-19 15:32:45 -0500

    logged in user

--- wp-content/themes/redprolid/include-header.php
+++ wp-content/themes/redprolid/include-header.php
@@ -20,7 +20,7 @@
               <?php 
                 if ( is_user_logged_in() ) {
 
-                   echo do_shortcode('[dm_login_form]'); 
+                   echo do_shortcode('[dm_login_formm]'); 
 
                 } else {
 
