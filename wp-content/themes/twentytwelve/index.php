21d24c21e70662a80901e8257041a888adf11a94
carlos calderon <cadermino@gmail.com>
2014-11-19 19:28:27 -0500

    disqus

--- wp-content/themes/twentytwelve/index.php
+++ wp-content/themes/twentytwelve/index.php
@@ -17,7 +17,7 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-		<div id="content" role="main" class="index">
+		<div id="content" role="main">
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
--- wp-content/themes/twentytwelve/page.php
+++ wp-content/themes/twentytwelve/page.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-		<div id="content" role="main" class="page">
+		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
