ce89af3e19a4dbe2e6ba38f42a4d3655fabe30c4
carlos calderon <cadermino@gmail.com>
2014-11-20 07:47:56 -0500

    comments

--- wp-content/themes/redprolid/comments.php
+++ wp-content/themes/redprolid/comments.php
@@ -22,28 +22,6 @@
 
 <div id="comments" class="comments-area">
 
-	<?php // You can start editing here -- including this comment! ?>
-
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-
-		</h2>
-
-		<ol class="commentlist">
-		</ol><!-- .commentlist -->
-
-
-
-		<?php
-		/* If there are no comments and comments are closed, let's leave a note.
-		 * But we only want the note on posts and pages that had comments in the first place.
-		 */
-		if ( ! comments_open() && get_comments_number() ) : ?>
-		
-		<?php endif; ?>
-
-	<?php endif; // have_comments() ?>
-
 	<?php comment_form(); ?>
 
 </div><!-- #comments .comments-area -->
