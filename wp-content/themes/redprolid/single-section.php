8b2fe9286f27a691f67b5b609417871456ee5b16
carlos calderon <cadermino@gmail.com>
2014-11-21 13:22:05 -0500

    comments

--- wp-content/themes/redprolid/single-section.php
+++ wp-content/themes/redprolid/single-section.php
@@ -5,8 +5,7 @@
     $segment2 = $elements[2] ; //For the 2nd segment
     $segment3 = $elements[3] ; //For the 3rd segment
 ?> 
-<?php //query_posts( 'category_name='.$segment2.'&posts_per_page=1' ); ?>   
-<?php //query_posts( 'category_name=puntos-de-vista' ); ?>
+<?php //query_posts( 'category_name='.$segment2.'&posts_per_page=1' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <div class="title">
     <h3><?php the_title(); ?></h3>
@@ -19,7 +18,8 @@
   <div class="footer">
     <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
   </div>
+<?php endwhile; ?>
   <div class="comments">
     <?php comments_template();?>
   </div>    
-<?php endwhile; ?>
+
