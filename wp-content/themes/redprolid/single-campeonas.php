12081551ccb6d3f71c8e6e058e4a2abf8eb04f2f
carlos calderon <cadermino@gmail.com>
2014-11-25 15:20:28 -0500

    single template

--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -1,30 +1,37 @@
 <section class="">
-  <div class="container text-justify with-hr">
+  <div class="container">
     <div class="row">
       <?php if( get_field('imagen_campeonas') ) { ?>
       <div class="col-sm-4 col-md-offset-1">
         <img src="<?php the_field('imagen_campeonas'); ?>" alt="" class="img-responsive">
       </div>
       <div class="col-sm-6">
-      <?php } else {?>  
-      <div class="col-sm-10 col-md-offset-1">
-      <?php } ?>  
+      <?php } else {?>
+      <div class="col-sm-10 col-md-offset-1">  
+      <?php } ?>
+        <h5>CAMPEONAS</h5>
         <h1 class="lh-lg"><?php the_title(); ?></h1>
-        <h3 class="light "><?php the_field('posicion_campeona'); ?></h3>
-        <p class="lh-lg text-justify">
-          <?php the_field('descripcion_campeona'); ?>
-        </p>
+        <h3 class="light lh-lg"><?php the_field('descripcion_campeona'); ?></h3>
+        <p class="lh-lg text-justify"></p>
       </div>
     </div>
   </div>
-  <div class="container text-justify with-hr">
-    <h4><?php //the_title(); ?></h4>
-    <p><?php the_field('contenido_campeona'); ?></p>
-    <div class="text-center">
-      <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
-    </div>          
+  <div class="container text-justify pb-70">
+    <div class="row">
+      <div class="col-sm-10 col-md-offset-1 with-hr">     
+        <h4><?php //the_title(); ?></h4>
+        <p><?php the_field('contenido_campeona'); ?></p>
+        <div class="text-center mt-70">
+          <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
+        </div>   
+      </div>
+    </div>       
   </div>
-  <div class="container text-justify with-hr">
-    <?php comments_template(); ?>
+  <div class="container text-justify pt-70 pb-70">
+    <div class="row">
+      <div class="col-sm-10 col-md-offset-1">  
+        <?php comments_template(); ?>
+      </div>
+    </div>
   </div>
 </section>
