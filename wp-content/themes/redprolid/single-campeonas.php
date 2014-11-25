4ceceae8e27c5db694f46960b129ed56cde45e7d
carlos calderon <cadermino@gmail.com>
2014-11-25 12:19:13 -0500

    single template

--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -1,12 +1,16 @@
 <section class="">
   <div class="container text-justify with-hr">
     <div class="row">
-      <div class="col-sm-3">
+      <?php if( get_field('imagen_campeonas') ) { ?>
+      <div class="col-sm-4 col-md-offset-1">
         <img src="<?php the_field('imagen_campeonas'); ?>" alt="" class="img-responsive">
       </div>
-      <div class="col-sm-9">
-        <h3><?php the_title(); ?></h3>
-        <p><?php the_field('posicion_campeona'); ?></p>
+      <div class="col-sm-6">
+      <?php } else {?>  
+      <div class="col-sm-10 col-md-offset-1">
+      <?php } ?>  
+        <h1 class="lh-lg"><?php the_title(); ?></h1>
+        <h3 class="light "><?php the_field('posicion_campeona'); ?></h3>
         <p class="lh-lg text-justify">
           <?php the_field('descripcion_campeona'); ?>
         </p>
