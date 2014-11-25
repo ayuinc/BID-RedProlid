ad57c20a178d5c4e0b05fd4c84ae94456ee38cbe
carlos calderon <cadermino@gmail.com>
2014-11-25 14:54:16 -0500

    single template

--- wp-content/themes/redprolid/page-noticias-list.php
+++ wp-content/themes/redprolid/page-noticias-list.php
@@ -39,18 +39,25 @@ get_header(); ?>
           <?php //die(the_post()); ?>
           <?php while ( have_posts() ) : the_post(); ?>
           <div class="row">
+            <div class="col-sm-1"></div>
             <?php if( get_field('imagen_noticias') ) { ?>
             <div class="col-sm-4 col-md-offset-1">
               <img src="<?php the_field('imagen_noticias'); ?>" alt="" class="img-responsive">
             </div>
             <div class="col-sm-6">
             <?php } else {?>
-            <div class="col-sm-10 col-md-offset-1">  
+            <div class="col-sm-10 with-hr">
             <?php } ?>
-              <h1 class="lh-lg"><?php the_title(); ?></h1>
-              <h3 class="light "><?php the_field('descripcion_rapida_noticias'); ?></h3>
-              <p class="lh-lg text-justify"></p>
+              <h3><?php the_title(); ?></h3>
+              <p><?php the_field('descripcion_rapida_noticias'); ?></p>
+              <h4></h4>
+              <p class="lh-lg text-justify"><?php the_field('contenido_noticias'); ?></p>
+              <!-- <p class="text-gray">Diciembre 2013</p> -->
+              <div class="text-right">
+                <a href="<?php echo get_permalink( get_the_ID() ); ?>">Lee el artículo >></a>
+              </div>
             </div>
+            <div class="col-sm-1"></div>
           </div>
           <?php endwhile; ?>
 
