9bdd344dc543abcebfed415aff39dfb91ae7a057
root <root@cxyg-ngsh.accessdomain.com>
2014-11-25 15:06:39 -0500

    cambios

--- wp-content/themes/redprolid/single-noticias.php
+++ wp-content/themes/redprolid/single-noticias.php
@@ -9,15 +9,16 @@
 	    <?php } else {?>
 	    <div class="col-sm-10 col-md-offset-1">  
       <?php } ?>
+      	<h5>NOTICIAS</h5>
         <h1 class="lh-lg"><?php the_title(); ?></h1>
-        <h3 class="light "><?php the_field('descripcion_rapida_noticias'); ?></h3>
+        <h3 class="llight lh-lg"><?php the_field('descripcion_rapida_noticias'); ?></h3>
         <p class="lh-lg text-justify"></p>
       </div>
     </div>
   </div>
-  <div class="container text-justify with-hr pb-70">
+  <div class="container text-justify pb-70">
 	  <div class="row">
-			<div class="col-sm-10 col-md-offset-1">  	  
+			<div class="col-sm-10 col-md-offset-1 with-hr">  	  
 		    <h4><?php //the_title(); ?></h4>
 		    <p><?php the_field('contenido_noticias'); ?></p>
 		    <div class="text-center mt-70">
