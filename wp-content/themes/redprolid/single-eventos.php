5ed478adcca46caee559d4e9f71d503f171c39f7
carlos calderon <cadermino@gmail.com>
2014-11-25 00:49:19 -0500

    single template

---
+++ wp-content/themes/redprolid/single-eventos.php
@@ -0,0 +1,26 @@
+<section class="">
+  <div class="container text-justify with-hr">
+    <div class="row">
+      <div class="col-sm-3">
+        <img src="<?php the_field('imagen_evento'); ?>" alt="" class="img-responsive">
+      </div>
+      <div class="col-sm-9">
+        <h3><?php the_title(); ?></h3>
+        <p><?php the_field('descripcion_evento'); ?></p>
+        <p class="lh-lg text-justify">
+          
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="container text-justify with-hr">
+    <h4><?php //the_title(); ?></h4>
+    <p><?php the_field('contenido_evento'); ?></p>
+    <div class="text-center">
+      <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
+    </div>          
+  </div>
+  <div class="container text-justify with-hr">
+    <?php comments_template(); ?>
+  </div>
+</section>
