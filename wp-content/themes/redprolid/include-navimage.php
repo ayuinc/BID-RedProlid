01b37f148537b1c502b3fc47979c173dfc80e62b
carlos calderon <cadermino@gmail.com>
2014-11-27 18:05:58 -0500

    concursos

--- wp-content/themes/redprolid/include-navimage.php
+++ wp-content/themes/redprolid/include-navimage.php
@@ -57,7 +57,7 @@
               </a>
             </li>
             <li>
-              <a href="<?php echo home_url('/'); ?>desarrolla-tu-liderazgo/pistas/">
+              <a href="<?php echo home_url('/'); ?>desarrolla-tu-liderazgo/la-pinta-no-es-lo-de-menos/">
                 <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-la-pinta-gs.jpg" alt="" class="gs">
                 <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-la-pinta-col.jpg" alt="" class="color">
                 <div class="title">                  
