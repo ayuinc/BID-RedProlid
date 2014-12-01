b4a941cc6da603640cf3036098ad5a82782739d6
Mercedes Zubieta <mercedes.zubieta@gmail.com>
2014-12-01 11:32:20 -0500

    ajustes del front en Adonde vamos

--- wp-content/themes/redprolid/page-dtl-seccion.php
+++ wp-content/themes/redprolid/page-dtl-seccion.php
@@ -27,51 +27,51 @@ get_header(); ?>
 							<?php if ($segment2 == "a-donde-vamos") : ?>
 		            <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-adonde-vamos-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #007c8d;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #007c8d;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle relative" width="560px" height="auto">
 							<?php endif; ?>
 							<?php if ($segment2 == "agenda-de-genero") : ?>
 		            <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-agenda-genero-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #c02f73;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #c02f73;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle relative" width="560px" height="auto">
 							<?php endif; ?>
 							<?php if ($segment2 == "carrera-de-vallas") : ?>
 								<div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-carrera-vallas-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #9a205c;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #9a205c;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">							
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle relative" width="560px" height="auto">							
 							<?php endif; ?>
 							<?php if ($segment2 == "la-pinta-no-es-lo-de-menos") : ?>
 								<div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-la-pinta-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #007c8d;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #007c8d;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle relative" width="560px" height="auto">
 							<?php endif; ?>
 							<?php if ($segment2 == "listas-para-la-politica") : ?>
 								<div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-listas-politica-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #015a71;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #015a71;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle relative" width="560px" height="auto">
 							<?php endif; ?>
 							<?php if ($segment2 == "nivelando-la-cancha") : ?>
 								<div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-nivelando-cancha-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #599a97;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #599a97;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-left mr-21 img-circle pt-7-100" width="560px" height="auto">
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-left mr-21 img-circle relative pt-7-100" width="560px" height="auto">
 							<?php endif; ?>
 							<?php if ($segment2 == "pistas-2") : ?>
 								<div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-pistas-icon.png" alt="<?php the_title(); ?>"></div>
 		            <h1 style="color: #007c8d;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #007c8d;">
+		            <hr style="border-top: 3px dotted #aaa;">
 		          </div>
-		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle" width="560px" height="auto">	
+		          <img src="<?php the_field('imagen-dtl-seccion'); ?>" alt="<?php the_title(); ?>" class="pull-right mr-21 img-circle relative" width="560px" height="auto">	
 							<?php endif; ?>
 		          <p class="text-left pt-14-100"><?php the_field('contenido-dtl-seccion'); ?></p>
 						<?php endwhile; ?>
