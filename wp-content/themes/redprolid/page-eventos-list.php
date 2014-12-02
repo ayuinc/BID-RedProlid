bb208503430d79e683e16811c470c8481290a9f0
carlos calderon <cadermino@gmail.com>
2014-12-02 10:40:26 -0500

    enterate

--- wp-content/themes/redprolid/page-eventos-list.php
+++ wp-content/themes/redprolid/page-eventos-list.php
@@ -8,50 +8,27 @@ get_header(); ?>
 <?php get_template_part( 'include', 'header' ); ?>
 <!--NAV-->
 <?php get_template_part( 'include', 'nav' ); ?>
-  <div class="mh-700">
-      <section id="puntos-de-vista-anteriores">
-        <div class="container">
-          <div class="panel panel-custom panel-highlight panel-flex">
-            <div class="panel-body">
-              <div class="pdv-icon">
-                <img src="../assets/img/pdv-main-icon.png" alt="">
-              </div>
-              <div class="col-sm-6 mb-ch-21">
-                <h1>Eventos</h1>
-                <a href="#" class="block">Eventos >></a>
-              </div>
-              <div>
-                <hr style="border-top: 3px dotted #aeab8b;">
-                <div class="col-sm-6">
-                  <?php //get_search_form(); ?>
-                </div>
-                <div class="col-sm-6 text-right">
-                </div>
-              </div>
-            </div>
-          </div>
+<div class="mh-700">
+  <section id="puntos-de-vista-anteriores">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 col-md-offset-1 mt-35 mb-35">
+          <h1>Eventos Anteriores</h1>
         </div>
-        <div class="container grid-block-lg">
-
+      </div>
+    </div>
+    <div class="container grid-block-lg">
+      <div class="row">
+        <div class="col-sm-10 col-md-offset-1">
           <?php query_posts( 'category_name=eventos' ); ?> 
-          <?php //die(the_post()); ?>
           <?php while ( have_posts() ) : the_post(); ?>
-          <div class="row">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-10 with-hr">
-              <h3><?php the_title(); ?></h3>
-              <p></p>
-              <h4></h4>
-              <p class="lh-lg text-justify"><?php the_field('descripcion_evento'); ?></p>
-              <!-- <p class="text-gray">Diciembre 2013</p> -->
-              <div class="text-right">
-                <a href="<?php echo get_permalink( get_the_ID() ); ?>">Lee el artículo >></a>
-              </div>
+            <div class="title">
+              <h3><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>
             </div>
-            <div class="col-sm-1"></div>
-          </div>
+            <div class="content mb-70">
+              <?php the_field('descripcion_evento'); ?>
+            </div>
           <?php endwhile; ?>
-
           <div class="text-center">
             <ul class="pager">
               <li><a href="#">Previos</a></li>
@@ -59,6 +36,8 @@ get_header(); ?>
             </ul>
           </div>
         </div>
-      </section>
+      </div>          
     </div>
+  </section>
+</div>
 <?php get_footer(); ?>
