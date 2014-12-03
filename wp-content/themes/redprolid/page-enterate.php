9493c399bfe655c25530560cd71d73d92233bbfd
carlos calderon <cadermino@gmail.com>
2014-12-03 16:34:14 -0500

    arreglos

--- wp-content/themes/redprolid/page-enterate.php
+++ wp-content/themes/redprolid/page-enterate.php
@@ -101,10 +101,10 @@ get_header(); ?>
             <div class="panel-body p-0 mt-42">
               <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                 <!-- Indicators -->
-                <ol class="carousel-indicators">
+                <!-- <ol class="carousel-indicators">
                   <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li> 
                   <li data-target="#carousel-example-generic" data-slide-to="1"></li> 
-                </ol>
+                </ol> -->
 
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner">
@@ -161,10 +161,10 @@ get_header(); ?>
                 </div>
 
                 <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev" style="width: 2%;">
                   <span class="icon-prev"></span>
                 </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" style="width: 2%;">
                   <span class="icon-next"></span>
                 </a>
               </div> <!-- END:CAROUSEL -->
