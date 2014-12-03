fd32c629a611807018e7c12886f0678d1690e3cf
carlos calderon <cadermino@gmail.com>
2014-12-03 16:06:46 -0500

    arreglos

--- wp-content/themes/redprolid/page-enterate.php
+++ wp-content/themes/redprolid/page-enterate.php
@@ -18,7 +18,7 @@ get_header(); ?>
             <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon.png" alt=""></div>
             <ul class="flex flex-row flex-space-between normalize-text pt-28">
               <li>
-                <h1 style="color: #aeab8b;">Entérate</h1>
+                <h1 class="mt-14" style="color: #aeab8b;">Entérate</h1>
               </li>
               <li class="text-right">
                 <a href="#" class="bold">¿Quieres compartir un recurso? &gt; &gt;</a>
@@ -29,11 +29,11 @@ get_header(); ?>
         </div>
         <div class="container with-shadow">
           <ul class="grid-list grid-list-centered grid-list-5 text-center">
-            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-1.png" alt=""><p>De dónde podrás encontrar todos los recursos y artículos</p></li>
-            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-2.png" alt=""><p>De las noticias y de los eventos que no podrás perderte </p></li>
-            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-3.png" alt=""><p>De lo que otras mujeres, con intereses similares, piensan</p></li>
-            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-4.png" alt=""><p>De diversos videos que podrás ver y compratir</p></li>
-            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-5.png" alt=""><p>De cómo estar conectada con lo que pasa en la Red PROLID</p></li>
+            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-1.png" alt=""><p style="color:##C4C09C">De dónde podrás encontrar todos los recursos y artículos</p></li>
+            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-2.png" alt=""><p style="color:##C4C09C">De las noticias y de los eventos que no podrás perderte </p></li>
+            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-3.png" alt=""><p style="color:##C4C09C">De lo que otras mujeres, con intereses similares, piensan</p></li>
+            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-4.png" alt=""><p style="color:##C4C09C">De diversos videos que podrás ver y compratir</p></li>
+            <li><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-grid-5.png" alt=""><p style="color:##C4C09C">De cómo estar conectada con lo que pasa en la Red PROLID</p></li>
           </ul>
           <p class="lead">Imagina esta sección como una biblioteca en la que almacenamos todo tipo de recursos de nuestra red:  libros, legislación, investigaciones, blogs y noticias, al igual que material audiovisual como entrevistas o vídeos. Queremos también incluir material eminentemente práctico como compendios de buenas prácticas, guías para las mujeres en el sector público o estudios de casos concretos. Aspiramos a que aquí puedas encontrar todo lo que puede serte útil en el desarrollo y fortalecimiento de tu liderazgo en el sector público y la política.</p>
         </div>
@@ -46,7 +46,7 @@ get_header(); ?>
                 <li class="icon" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/icons/sprites-home-grid.png); background-repeat: no-repeat; background-position: 0px 0px;"></li>
               </ul>
             </div>
-            <div class="panel-body">
+            <div class="panel-body pt-0">
               <div class="col-sm-4 pt-35">
                 <div class="enterate-search bg-panel p-14">
                   <h1>Búsqueda</h1>
@@ -77,6 +77,7 @@ get_header(); ?>
 			                    <h5><?php the_title(); ?></h5>
 			                    <small class="date"><?php echo get_the_date(); ?></small> <a href="<?php echo get_permalink( get_the_ID() ); ?>" class="text-primary">Lee más &gt;&gt;</a>
 			                  </li>
+                        <hr>
 			                 <?php endforeach; ?>
 				            <?php endwhile; ?>
 									<?php endif; ?>  
@@ -86,7 +87,7 @@ get_header(); ?>
           </div>
         </div>
       </section>
-      <section class="bg-panel mh-350">
+      <section class="bg-panel mh-350 mt-49">
         <div class="container">
           <div class="panel panel-custom">
             <div class="panel-heading">
