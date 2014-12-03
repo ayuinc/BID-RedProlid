3da84b096c08b838ad52d75cc33e9716636dc2a7
carlos calderon <cadermino@gmail.com>
2014-12-03 03:46:50 -0500

    arreglos

--- wp-content/themes/redprolid/page-concursos-list.php
+++ wp-content/themes/redprolid/page-concursos-list.php
@@ -9,56 +9,56 @@ get_header(); ?>
 <!--NAV-->
 <?php get_template_part( 'include', 'nav' ); ?>
   <div class="mh-700">
-      <section id="puntos-de-vista-anteriores">
-        <div class="container">
-          <div class="panel panel-custom panel-highlight panel-flex">
-            <div class="panel-body">
-              <div class="pdv-icon">
-                <img src="../assets/img/pdv-main-icon.png" alt="">
+    <section id="puntos-de-vista-anteriores">
+      <div class="container">
+        <div class="panel panel-custom panel-highlight panel-flex">
+          <div class="panel-body">
+            <div class="pdv-icon">
+              <img src="../assets/img/pdv-main-icon.png" alt="">
+            </div>
+            <div class="col-sm-6 mb-ch-21">
+              <h1>Concursos</h1>
+              <a href="#" class="block">Concursos >></a>
+            </div>
+            <div>
+              <hr style="border-top: 3px dotted #aeab8b;">
+              <div class="col-sm-6">
+                <?php //get_search_form(); ?>
               </div>
-              <div class="col-sm-6 mb-ch-21">
-                <h1>Concursos</h1>
-                <a href="#" class="block">Concursos >></a>
-              </div>
-              <div>
-                <hr style="border-top: 3px dotted #aeab8b;">
-                <div class="col-sm-6">
-                  <?php //get_search_form(); ?>
-                </div>
-                <div class="col-sm-6 text-right">
-                </div>
+              <div class="col-sm-6 text-right">
               </div>
             </div>
           </div>
         </div>
-        <div class="container grid-block-lg">
+      </div>
+      <div class="container grid-block-lg">
 
-          <?php query_posts( 'category_name=concursos' ); ?> 
-          <?php //die(the_post()); ?>
-          <?php while ( have_posts() ) : the_post(); ?>
-          <div class="row">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-10 with-hr">
-              <h3><?php the_title(); ?></h3>
-              <p></p>
-              <h4></h4>
-              <p class="lh-lg text-justify"><?php the_field('descripcion_concurso'); ?></p>
-              <!-- <p class="text-gray">Diciembre 2013</p> -->
-              <div class="text-right">
-                <a href="<?php echo get_permalink( get_the_ID() ); ?>">Lee el artículo >></a>
-              </div>
+        <?php query_posts( 'category_name=concursos' ); ?> 
+        <?php //die(the_post()); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+        <div class="row">
+          <div class="col-sm-1"></div>
+          <div class="col-sm-10 with-hr">
+            <h3><?php the_title(); ?></h3>
+            <p></p>
+            <h4></h4>
+            <p class="lh-lg text-justify"><?php the_field('descripcion_concurso'); ?></p>
+            <!-- <p class="text-gray">Diciembre 2013</p> -->
+            <div class="text-right">
+              <a href="<?php echo get_permalink( get_the_ID() ); ?>">Lee el artículo >></a>
             </div>
-            <div class="col-sm-1"></div>
           </div>
-          <?php endwhile; ?>
-
-          <div class="text-center">
-            <ul class="pager">
-              <li><a href="#">Previos</a></li>
-              <li><a href="#">Siguiente</a></li>
-            </ul>
-          </div>
+          <div class="col-sm-1"></div>
         </div>
-      </section>
-    </div>
+        <?php endwhile; ?>
+
+        <div class="text-center">
+          <ul class="pager">
+            <li><a href="#">Previos</a></li>
+            <li><a href="#">Siguiente</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
 <?php get_footer(); ?>
