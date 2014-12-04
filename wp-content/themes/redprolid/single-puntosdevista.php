a9998db0c18dd20a3526bb83764eb987df44a7c2
carlos calderon <cadermino@gmail.com>
2014-12-04 15:50:55 -0500

    arreglos

--- wp-content/themes/redprolid/single-puntosdevista.php
+++ wp-content/themes/redprolid/single-puntosdevista.php
@@ -60,7 +60,7 @@
 		    <ul class="grid-list grid-list-2 pb-ch-7">
         <?php
         $category = get_the_category(get_the_ID());
-        die(print_r($category));  
+        // die(print_r($category));  
         $args = array(
           'number' => '3'
         );
@@ -70,11 +70,11 @@
         <?php 
           $comm_post_id = $comment->comment_post_ID;   
         ?>
-        <?php  //if ( $category->slug == "puntos-de-vista")){ ?>
+        <?php if ( $category[0]->slug == "puntos-de-vista")) : ?>
 
             <li><a href="<?php echo get_permalink( $comm_post_id ); ?>"><?php echo($comment->comment_content);?></a>/ <?php echo($comment->comment_author);?></li>          
 
-        <?php //} ?>
+        <?php endif ?>
           
         <?php endforeach;?>
         </ul>
