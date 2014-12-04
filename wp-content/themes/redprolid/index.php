c00da16eb260d23c1f3e175c0a20132c0118c5cc
andreadelrio <delrio.andre@gmail.com>
2014-12-04 16:51:59 -0500

    fix vote button

--- wp-content/themes/redprolid/index.php
+++ wp-content/themes/redprolid/index.php
@@ -166,7 +166,7 @@ get_header(); ?>
 									<?php while ( have_posts() ) : the_post(); ?>  
                   <div class="col-sm-12 panel-lost pl-0">
                     <?php the_content(); ?>
-                    <div class="pull-left">
+                    <div class="pull-left view-results">
                       <a href="http://www.redprolid.org/poll">Ver resultados >></a>
                     </div>
                   </div>
