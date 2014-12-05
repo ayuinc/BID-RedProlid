e515528d6ccca4a7b6979ce189434415f1f1497c
andreadelrio <delrio.andre@gmail.com>
2014-12-05 15:18:50 -0500

    add pieza icon

--- wp-content/themes/redprolid/page-registrate.php
+++ wp-content/themes/redprolid/page-registrate.php
@@ -168,13 +168,13 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 		<section class="">
 		  <div class="container">
-	      <div class="absolute dtl-custom-heading" style="padding-top: 6px;">
-	      	<div>
-          	<img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/registrate.png" class="pull-left mr-21 relative pt-70 mt-42" width="70" height="auto">
+		    <div class="row mt-35">
+		      <div class="col-sm-10 col-md-offset-1"> 
 						<h1 style="color: #aeab8b;"><?php the_title(); ?></h1>
 						<hr style="border-top: 3px dotted #aaa;">
-	      	</div>
-	      </div>
+            <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/registrate.png" class="pull-left mr-21 relative pt-70 mt-42" width="60px" height="auto">
+		      </div>
+		    </div>
 		  </div>
 		  <div class="container-fluid text-justify with-hr pb-70">
 			  <div class="row">
