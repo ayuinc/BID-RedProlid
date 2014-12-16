6a054395b24d528389c98c17eb0b77d8c81685cf
root <root@cxyg-ngsh.accessdomain.com>
2014-12-15 21:09:25 -0500

    Ajustes

--- wp-content/themes/redprolid/single-section.php
+++ wp-content/themes/redprolid/single-section.php
@@ -17,15 +17,7 @@ foreach( $customposts as $post ) :  // start you custom loop
     <h3><?php the_title(); ?></h3>
     <p>
       FUENTE: <?php the_field('fuente-debates'); ?>
-<<<<<<< HEAD
     </p>
-=======
-    <?php endif; ?>
-    <?php if ($segment2 == 'agenda-de-genero' || $segment2 == 'carrera-de-vallas' || $segment2 == 'nivelando-la-cancha') {?>
-      <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/enterate-grid-4-01.png" alt="" style="width:20px">
-    <?php } ?>
-    </div>
->>>>>>> 123d32f746896740702f35bf0a6cfba37d3618e5
     </div>
     <div class="content">
       <?php the_field('contenido-debates'); ?>
