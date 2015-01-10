6e381bd65384d1bc2ed5d4aa67ff5622c4673c3c
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-09 19:17:09 -0500

    Cambios single video

--- wp-content/themes/redprolid/single-video.php
+++ wp-content/themes/redprolid/single-video.php
@@ -9,10 +9,11 @@
   <div class="container mt-35 mb-35">
     <div class="row">
 	    <div class="col-sm-10 col-md-offset-1">  
-      	<small>VIDEO</small>
-        <h2 class="lh-lg"><?php the_title(); ?></h2>
+        <h2 class="medium"><?php the_title(); ?></h2>
         <iframe width="100%" height="350" src="//www.youtube.com/embed/<?php the_field('id_video'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>
-        <p><?php the_field('descripcion_video'); ?></p> 
+        <p class="mt-14"><?php the_field('descripcion_video'); ?></p> 
+				<?php $tempDate = get_the_date(); ?>
+				<small><?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?></small>        
 		    <div class="text-center mt-35 mb-35 ">
 		      <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
 		    </div>   
