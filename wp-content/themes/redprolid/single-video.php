5519e8655952cacd62fd6917942c537e5e3e9e5a
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-09 19:15:58 -0500

    Cambios single video

--- wp-content/themes/redprolid/single-video.php
+++ wp-content/themes/redprolid/single-video.php
@@ -1,4 +1,3 @@
-asdfads
 <section class="">
   <div class="container mt-35">
     <div class="row">
@@ -9,24 +8,11 @@ asdfads
   </div>		
   <div class="container mt-35 mb-35">
     <div class="row">
-	    <?php if( get_field('imagen_noticias') ) { ?>
-      <div class="col-sm-3 col-md-offset-1">
-        <img src="<?php the_field('imagen_noticias'); ?>" alt="" class="img-responsive">
-      </div>
-      <div class="col-sm-7">
-	    <?php } else {?>
 	    <div class="col-sm-10 col-md-offset-1">  
-      <?php } ?>
       	<small>VIDEO</small>
         <h2 class="lh-lg"><?php the_title(); ?></h2>
-        <h4 class="light lh-lg"><?php the_field('descripcion_rapida_noticias'); ?></h4>
-      </div>
-    </div>
-  </div>
-  <div class="container text-left pb-70">
-	  <div class="row">
-			<div class="col-sm-10 col-md-offset-1 with-hr">  	  
-		    <p><?php the_field('contenido_noticias'); ?></p>
+        <iframe width="100%" height="350" src="//www.youtube.com/embed/<?php the_field('id_video'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>
+        <p><?php the_field('descripcion_video'); ?></p> 
 		    <div class="text-center mt-35 mb-35 ">
 		      <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
 		    </div>   
