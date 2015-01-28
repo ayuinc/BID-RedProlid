569d482bf51f7f9452793577fae0ebe0ee1fc162
Gus <yo@soygus.com>
2015-01-28 14:53:03 -0500

    HEADER: normalized text

--- wp-content/themes/redprolid/page-foro-resumen.php
+++ wp-content/themes/redprolid/page-foro-resumen.php
@@ -14,14 +14,10 @@ get_header(); ?>
 <div>
 	<section class="mt-7 mb-21 pb-70"> 
 	  <div class="container relative mb-14">
-	    <div class="row">
-	      <div class="col-md-12">
-	        <?php the_breadcrumb(); ?>
-	      </div>
-	    </div>
+	    <?php the_breadcrumb(); ?>	    
 	    <div class="clearfix sub-header bg-panel">
         <div class="col-md-6">
-          <h1 class="brand-titular">Resumen del foro</h1>
+          <h1>Resumen del foro</h1>
         </div>
         <div class="col-md-6">
           <nav class="text-right">
@@ -29,15 +25,15 @@ get_header(); ?>
           </nav>
         </div>
 	    </div> 
-	  </div>
-		<div class="ph-ch-70 pb-70">
-			<h3>Mujeres en la política</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam hic amet quos a temporibus, in quibusdam voluptatibus neque nobis nihil tempore eveniet officia, adipisci minima assumenda voluptatem. Eius, quisquam suscipit.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui enim deleniti voluptas culpa sint delectus eum quisquam veniam, ratione aliquam quam! Illo distinctio recusandae, qui reprehenderit ut neque, ad ea!</p>
-			<div class="text-right">
-				<a href="#" class="btn btn-primary">Ver todo el documento</a>
+			<div class="ph-70 pb-70">
+				<h3>Mujeres en la política</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam hic amet quos a temporibus, in quibusdam voluptatibus neque nobis nihil tempore eveniet officia, adipisci minima assumenda voluptatem. Eius, quisquam suscipit.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui enim deleniti voluptas culpa sint delectus eum quisquam veniam, ratione aliquam quam! Illo distinctio recusandae, qui reprehenderit ut neque, ad ea!</p>
+				<div class="text-right">
+					<a href="#" class="btn btn-primary">Ver todo el documento</a>
+				</div>
 			</div>
-		</div>
+	  </div>
 	</section>
 </div>
 
