7e9cbe1e63c5b76b4f02f459a8cb5be133e1c58a
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-02-05 14:30:04 -0500

    cambios en menu de DTL

--- wp-content/themes/redprolid/index.php
+++ wp-content/themes/redprolid/index.php
@@ -82,11 +82,11 @@ get_header(); ?>
             <div class="carousel-inner" role="listbox">
               <div class="item active">
                 <ul class="grid-list grid-list-4 grid-list-1-xs dtl-carousel">
-			          	<?php query_posts( 'category_name=home-desarrolla-tu-liderazgo&posts_per_page=3' ); ?>	
+			          	<?php query_posts( 'category_name=home-desarrolla-tu-liderazgo' ); ?>	
 									<?php while ( have_posts() ) : the_post(); ?>  	                
                   <li>
                     <div class="dtl-item" data-href="#">
-                      <div class="bg-img-block-no-cover bg-img-block bg-dtl-adonde-vamos" style="background-image: url(<?php the_field('imagen_home_dtl'); ?>);"></div>
+                      <div class="bg-img-block-no-cover bg-img-block" style="background-image: url(<?php the_field('imagen_home_dtl'); ?>);"></div>
 
                       <div class="dtl-title">
                         <h3><?php the_title(); ?></h3>
