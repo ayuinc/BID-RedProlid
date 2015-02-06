1833ca0652cd501f1aba137040e7a0c8e3382f5c
Gus <yo@soygus.com>
2015-02-06 17:06:41 -0500

    CONCURSOS: layout

--- wp-content/themes/redprolid/single-concurso.php
+++ wp-content/themes/redprolid/single-concurso.php
@@ -1,37 +1,53 @@
 <section>
   <div class="container">
 		<?php the_breadcrumb(); ?>
-    <div class="row pv-21">
-      <?php if( get_field('imagen_concurso') ) { ?>
-      <div class="col-sm-3 col-md-offset-1">
-        <img src="<?php the_field('imagen_concurso'); ?>" alt="" class="img-responsive">
-      </div>
+    <div class="clearfix bg-panel sub-header">
       <div class="col-sm-7">
-      <?php } else {?>
-      <div class="col-sm-10 col-md-offset-1">  
-      <?php } ?>
-        <h2 class="lh-lg"><?php the_title(); ?></h2>
-        <h4 class="light lh-lg"><?php the_field('descripcion_concurso'); ?></h4>
+        <div>
+          <h1>Concurso blablabla</h1>
+          <nav class="mt-21">
+            <a href="<?php echo content_url('/'); ?>concursos">Noticias >></a>
+          </nav>
+        </div>
+      </div>
+      <div class="sub-header-icon">
+        <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/concursos-icon-circle-alpha.png" width="94px" height="auto" alt="">
       </div>
     </div>
-    <div class="row">
-      <div class="col-sm-10 col-md-offset-1 with-hr">     
-        <p><?php the_field('contenido_concurso'); ?></p>
-        <a name="terminos-y-condiciones"></a>
-        <p class="text-center"><button id="boton_entrevista_full" class="btn btn-primary">Lee los Términos y Condiciones</button></p>
-        <div id="entrevista_full">
-        	<h4 class="medium">Términos y condiciones</h4>
-					<p><?php the_field('terminos_concurso'); ?></p>
+    <div class="ph-70">
+      <div class="row">
+        <?php if( get_field('imagen_concurso') ) { ?>
+        <div class="col-sm-3">
+          <img src="<?php the_field('imagen_concurso'); ?>" alt="" class="img-responsive">
         </div>
-        <div class="text-center">
-          <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
-        </div>   
+        <div class="col-sm-7">
+        <?php } else {?>
+        <div class="col-sm-12">  
+        <?php } ?>
+          <h2 class="lh-lg"><?php the_title(); ?></h2>
+          <h4 class="light lh-lg"><?php the_field('descripcion_concurso'); ?></h4>
+        </div>
       </div>
-    </div>  
-    <div class="row">
-      <div class="col-sm-10 col-md-offset-1">  
-        <?php comments_template(); ?>
-      </div>
+      <div class="row">
+        <div class="col-sm-10 col-md-offset-1 with-hr">     
+          <p><?php the_field('contenido_concurso'); ?></p>
+          <a name="terminos-y-condiciones"></a>
+          <div class="content-display">
+            <div class="text-right mt-14 mb-14">
+              <a class="btn btn-primary" id="boton_entrevista_full" >Lee los Términos y Condiciones</a>
+              <a class="btn btn-primary" id="boton_ocultar_entrevista_full" >Ocultar la entrevista completa</a>
+            </div>
+          </div>
+          <div id="entrevista_full">
+          	<h4 class="medium">Términos y condiciones</h4>
+  					<p><?php the_field('terminos_concurso'); ?></p>
+          </div>
+          <div class="text-right">
+            <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
+          </div>   
+        </div>
+      </div>  
+      <?php comments_template(); ?>
     </div>
   </div>
 </section>
