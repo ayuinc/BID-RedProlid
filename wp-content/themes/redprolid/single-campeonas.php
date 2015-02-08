456f090bf545419e6c7df74d7490663bef3d875c
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-02-08 17:55:23 -0500

    Incluir modal en Campeonas.

--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -11,7 +11,7 @@
       <div class="col-sm-7 col-xs-12">
         <nav class="text-right text-center-xs">
           <a href="<?php echo home_url('/'); ?>campeonas/campeonas-anteriores">¿Quieres leer nuestras entrevistas anteriores?</a> <span class="text-primary">|</span>
-          <a href="<?php echo home_url('/'); ?>compartir-campeonas">Nos propones un tema</a>
+          <a href="#" data-toggle="modal" data-target="#modalCampeonas">Nos propones un tema</a>
         </nav>
       </div>
     </div>
@@ -47,3 +47,57 @@
     </div>
   </div>      
 </section>
+
+	<!-- Modal EVENTOS -->
+	<div class="modal fade" id="modalCampeonas" tabindex="-1" role="dialog" aria-labelledby="modalCampeonasLabel" aria-hidden="true">
+		<?php if ( is_user_logged_in() ) { ?>
+  	<div class="modal-dialog">
+  	<?php } else { ?>
+  	<div class="modal-dialog modal-lg">
+  	<?php } ?>
+	    <div class="modal-content bg-panel">
+	      <div class="modal-body">
+	        <div class="clearfix sub-header sub-header-sm">
+		        <div class="col-sm-1 col-xs-3">
+		          <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-topic-icon.png" alt="" width="100%"></div>
+		        </div>
+		        <div class="col-sm-5 col-xs-9"></div>
+		        <div class="col-sm-6 col-xs-12"></div>
+		      </div>
+		      <?php if ( is_user_logged_in() ) { ?>
+		      	<?php if( function_exists( 'ninja_forms_display_form' ) ) { ?>
+		      		<p>¿Nos propones un tema para desarrollar en nuestras entrevistas y/o a quíen te gustaría que entrevistemos?</p>
+							<?php ninja_forms_display_form( 5 ); ?>
+						<?php } ?>
+					<?php } else { ?>
+						<div class="row">
+				  		<div class="col-sm-6 col-sm-offset-3">
+				  			<div class="user-sign-in-form pv-21">
+					  			<h2 class="medium">Ingresa al sistema</h2>
+			            <?php 
+			              if ( is_user_logged_in() ) {
+
+			                $current_user = wp_get_current_user();
+			                echo '<div class="text-right">';
+			                echo '<h4 class="light">Hola '.$current_user->user_nicename.'</h4>';
+											echo '<a href="'.wp_logout_url().'" title="Logout" class="light">Cierra tu sesión</a>';
+			                echo '</div>';
+
+			              } else {
+
+			                echo do_shortcode('[dm_login_form]'); 
+
+			              }
+			            ?>
+			           </div>
+				  		</div>
+				  	</div>
+			  	<?php } ?>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar X</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- END Modal EVENTOS-->	
