89694c3e18cae5710a86661db2c22386e94840b5
Herman Marin <herman.marin@ayuinc.com>
2015-02-09 09:56:13 -0500

    Cambios webinarios

--- wp-content/themes/redprolid/single-webinario.php
+++ wp-content/themes/redprolid/single-webinario.php
@@ -93,8 +93,7 @@
 		      </div>
 		      <?php if ( is_user_logged_in() ) { ?>
 		      	<?php if( function_exists( 'ninja_forms_display_form' ) ) { ?>
-		      		<p>¿Nos propones un tema para desarrollar en nuestras entrevistas y/o a quíen te gustaría que entrevistemos?</p>
-							<?php ninja_forms_display_form( 5 ); ?>
+							<?php ninja_forms_display_form( 7 ); ?>
 						<?php } ?>
 					<?php } else { ?>
 						<div class="row">
