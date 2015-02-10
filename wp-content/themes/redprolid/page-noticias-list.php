99cdb93d11339adb83f4d64ee798069aaa842ffd
Herman Marin <herman.marin@ayuinc.com>
2015-02-10 11:07:25 -0500

    Cambios en ultimas noticias

--- wp-content/themes/redprolid/page-noticias-list.php
+++ wp-content/themes/redprolid/page-noticias-list.php
@@ -35,7 +35,7 @@ get_header(); ?>
             </div>
             <div class="content mb-7">
               <?php the_field('descripcion_rapida_noticias'); ?>
-							<p><a href="<?php echo get_permalink( get_the_ID() ); ?>" class="medium">Lee la noticia >></a></p>
+							<p class="text-right"><a href="<?php echo get_permalink( get_the_ID() ); ?>" class="medium">Lee la noticia >></a></p>
             </div>
             <hr>
           <?php endwhile; ?>
