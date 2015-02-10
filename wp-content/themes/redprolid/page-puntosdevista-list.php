11d55dff304c1ef60f990e82e3b758122db6ca0b
Herman Marin <herman.marin@ayuinc.com>
2015-02-10 09:18:59 -0500

    Ajuste en Puntos de Vista

--- wp-content/themes/redprolid/page-puntosdevista-list.php
+++ wp-content/themes/redprolid/page-puntosdevista-list.php
@@ -41,3 +41,62 @@ get_header(); ?>
   </section>
 </div>
 <?php get_footer(); ?>
+
+
+<?php
+/*
+Template Name: Puntos de vista list
+*/
+get_header(); ?>
+
+<!--HEADER-->
+<?php get_template_part( 'include', 'header' ); ?>
+<!--NAV-->
+<?php get_template_part( 'include', 'nav' ); ?>
+<section id="puntos-de-vista-anteriores">
+  <div class="container">
+    <?php the_breadcrumb(); ?>
+    <div class="clearfix bg-panel sub-header">
+      <div class="col-sm-7">
+        <div>
+          <h1>Puntos de vista anteriores</h1>
+          <nav class="mt-21">
+            <a href="<?php echo content_url('/'); ?>noticias">Puntos de vista >></a>
+          </nav>
+        </div>
+      </div>
+      <div class="sub-header-icon">
+        <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon-alpha.png" width="94px" height="auto" alt="">
+      </div>
+    </div>	      
+    <div class="ph-70">
+      <div class="row">
+        <div class="col-sm-12">
+          <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+          <?php query_posts( 'category_name=puntos-de-vista&posts_per_page=10&paged=' . $paged ); ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+            <div class="title">
+              <h3 class="medium mb-0"><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>
+            </div>
+            <div class="content mb-7">
+              <?php the_field('descripcion_rapida_noticias'); ?>
+							<p><a href="<?php echo get_permalink( get_the_ID() ); ?>" class="medium">Lee el artículo >></a></p>
+            </div>
+            <hr>
+          <?php endwhile; ?>
+          <div class="text-center">
+            <ul class="pager">
+              <li><?php next_posts_link( 'Anteriores' ); ?></li>
+              <li><?php previous_posts_link( 'Posteriores' ); ?></li>
+            </ul>
+          </div>          
+        </div>
+      </div>          
+    </div>
+  </div>
+</section>
+<section>  
+  <div class="container p-21">
+  </div>
+</section>
+<?php get_footer(); ?>
