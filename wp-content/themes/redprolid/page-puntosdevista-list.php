0188698231fc83ba3aec13979e6f677ea0390522
Herman Marin <herman.marin@ayuinc.com>
2015-02-10 09:20:35 -0500

    Ajuste en Puntos de Vista

--- wp-content/themes/redprolid/page-puntosdevista-list.php
+++ wp-content/themes/redprolid/page-puntosdevista-list.php
@@ -8,51 +8,6 @@ get_header(); ?>
 <?php get_template_part( 'include', 'header' ); ?>
 <!--NAV-->
 <?php get_template_part( 'include', 'nav' ); ?>
-<div class="mh-700">
-  <section id="puntos-de-vista-anteriores">
-    <div class="container">
-      <div class="clearfix bg-panel sub-header">
-        <div class="col-sm-7">
-          <div>
-            <h1>Puntos de vista anteriores</h1>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-10 col-md-offset-1">
-          <?php query_posts( 'category_name=puntos-de-vista' ); ?> 
-          <?php while ( have_posts() ) : the_post(); ?>
-            <div class="title">
-              <h3><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>
-            </div>
-            <div class="content mb-70">
-              <?php the_field('contenido_punto_de_vista'); ?>
-            </div>
-          <?php endwhile; ?>
-          <div class="text-center">
-            <ul class="pager">
-              <li><a href="#">Previos</a></li>
-              <li><a href="#">Siguiente</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>          
-    </div>
-  </section>
-</div>
-<?php get_footer(); ?>
-
-
-<?php
-/*
-Template Name: Puntos de vista list
-*/
-get_header(); ?>
-
-<!--HEADER-->
-<?php get_template_part( 'include', 'header' ); ?>
-<!--NAV-->
-<?php get_template_part( 'include', 'nav' ); ?>
 <section id="puntos-de-vista-anteriores">
   <div class="container">
     <?php the_breadcrumb(); ?>
