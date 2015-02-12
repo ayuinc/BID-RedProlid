a7458955def0e5ac2cd31ca7c2fcd950491e64c7
Herman Marin <herman.marin@ayuinc.com>
2015-02-12 14:54:32 -0500

    Cambios

--- wp-content/themes/redprolid/include-dtl-carousel.php
+++ wp-content/themes/redprolid/include-dtl-carousel.php
@@ -6,7 +6,7 @@
       <li>
         <div class="dtl-item" data-href="<?php the_field('link_a_seccion_interna_home_dtl'); ?>">
           <div class="bg-img-block-no-cover bg-img-block" style="background-image: url(<?php the_field('imagen_home_dtl'); ?>);"></div>
-          <div class="dtl-title">
+          <div class="dtl-title p-0">
             <h3><?php the_title(); ?></h3>
             <div class="text-center">
               <a href="<?php the_field('link_a_seccion_interna_home_dtl'); ?>" class="btn btn-xs btn-primary">Ve más</a>
