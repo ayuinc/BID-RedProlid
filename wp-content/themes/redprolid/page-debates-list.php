e3c27d8110606c0ef0a2649b1de35bdb58750afd
Herman Marin <herman.marin@ayuinc.com>
2015-02-13 17:58:45 -0500

    Ajustes categorias dtl

--- wp-content/themes/redprolid/page-debates-list.php
+++ wp-content/themes/redprolid/page-debates-list.php
@@ -20,9 +20,9 @@ get_header(); ?>
 		<div class="ph-70">
       <div class="row">
       	<div class="col-sm-10 col-md-offset-1">
-	      	<?php $categoria = $_GET['categoria']; ?>
+	      	<?php echo $categoria = $_GET['categoria']; ?>
           <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
-          <?php query_posts( 'category_name='.$categoria.'&posts_per_page=10&paged=' . $paged ); ?>          
+          <?php query_posts( 'category_name=dtl-debates&posts_per_page=10&paged=' . $paged ); ?>          
           <?php while ( have_posts() ) : the_post(); ?>
             <div class="title">
               <h3><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>
