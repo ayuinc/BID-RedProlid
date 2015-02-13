8f1c30a488224f911c50740ae0972c6de01319ec
Herman Marin <herman.marin@ayuinc.com>
2015-02-13 18:05:16 -0500

    Ajustes categorias dtl

--- wp-content/themes/redprolid/page-debates-list.php
+++ wp-content/themes/redprolid/page-debates-list.php
@@ -10,17 +10,25 @@ get_header(); ?>
 <?php get_template_part( 'include', 'nav' ); ?>
 <section id="puntos-de-vista-anteriores">
   <div class="container">
-  	<div class="clearfix bg-panel sub-header">
+    <?php the_breadcrumb(); ?>
+    <div class="clearfix bg-panel sub-header">
       <div class="col-sm-7">
         <div>
-          <h1 style="color: #aeab8b;" class="">Debates anteriores</h1>
+          <h1>Debates anteriores</h1>
+          <nav class="mt-21">
+            <a href="<?php echo content_url('/'); ?>noticias">Noticias >></a>
+          </nav>
         </div>
       </div>
-    </div>  
-		<div class="ph-70">
+      <div class="sub-header-icon">
+        <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon-alpha.png" width="94px" height="auto" alt="">
+      </div>
+    </div>	      
+    <div class="ph-70">
       <div class="row">
-      	<div class="col-sm-10 col-md-offset-1">
-          <?php query_posts( 'category_name=dtl-debates' ); ?> 
+        <div class="col-sm-12">
+          <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+          <?php query_posts( 'category_name=dtl-debates&posts_per_page=10&paged=' . $paged ); ?>
           <?php while ( have_posts() ) : the_post(); ?>
             <div class="title">
               <h3><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>
@@ -36,16 +44,21 @@ get_header(); ?>
             <div class="content mb-70">
               <?php the_field('contenido-debates'); ?>
             </div>
+            <hr>
           <?php endwhile; ?>
           <div class="text-center">
             <ul class="pager">
-              <li><a href="#">Previos</a></li>
-              <li><a href="#">Siguiente</a></li>
+              <li><?php next_posts_link( 'Anteriores' ); ?></li>
+              <li><?php previous_posts_link( 'Posteriores' ); ?></li>
             </ul>
-          </div>
-      	</div>
-      </div>	        
+          </div>          
+        </div>
+      </div>          
     </div>
+  </div>
+</section>
+<section>  
+  <div class="container p-21">
   </div>
 </section>
 <?php get_footer(); ?>
