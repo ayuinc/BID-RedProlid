52ca6acf6bc3d654d8b520b7d471493cae203c5e
Herman Marin <herman.marin@ayuinc.com>
2015-02-15 15:15:03 -0500

    Carousel DTl

--- wp-content/themes/redprolid/page-dtl-seccion.php
+++ wp-content/themes/redprolid/page-dtl-seccion.php
@@ -34,7 +34,7 @@ get_header(); ?>
 								<div class="col-sm-1 col-xs-3">
 				          <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-adonde-vamos-icon.png"></div>
 				        </div>
-				        <div class="col-sm-5 col-xs-9">
+				        <div class="col-sm-6 col-xs-9">
 				          <h1 class="title-a-donde-vamos"><?php the_title(); ?></h1>
 				        </div>
 		          </div>
