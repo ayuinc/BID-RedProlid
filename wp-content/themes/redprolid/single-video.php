c6db8bfc9842b6517e32e11f680ac3e532b30310
Herman Marin <herman.marin@ayuinc.com>
2015-02-15 14:30:43 -0500

    COntrol de datos noticias

--- wp-content/themes/redprolid/single-video.php
+++ wp-content/themes/redprolid/single-video.php
@@ -10,8 +10,6 @@
     </div>
     <div class="ph-70">
 	    <h2 class="medium"><?php the_title(); ?></h2>
-	    <iframe width="100%" height="420" src="//www.youtube.com/embed/<?php the_field('id_video'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>
-	    <p class="mt-14"><?php the_field('descripcion_video'); ?></p> 
 			<?php $tempDate = get_the_date(); ?>
 			<p>
 				<?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?> 
@@ -19,7 +17,9 @@
 				<?php if ($pais_video!='') { ?>
 				, <strong><?php echo $pais_video; ?></strong>
 				<?php } ?>
-			</p>        
+			</p> 	    
+	    <iframe width="100%" height="420" src="//www.youtube.com/embed/<?php the_field('id_video'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>     
+	    <p class="mt-14"><?php the_field('descripcion_video'); ?></p>			   
 	    <div class="text-center mt-7 mb-7 ">
 	      <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
 	    </div>   
