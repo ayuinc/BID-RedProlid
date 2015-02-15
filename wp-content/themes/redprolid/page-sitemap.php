60b1a704603de8f3a260db8fbd8f77b824575c03
Herman Marin <herman.marin@ayuinc.com>
2015-02-15 12:27:00 -0500

    Ajuste Sitemap

--- wp-content/themes/redprolid/page-sitemap.php
+++ wp-content/themes/redprolid/page-sitemap.php
@@ -15,7 +15,10 @@ get_header(); ?>
     <div class="container">
       <?php the_breadcrumb(); ?> 
       <div class="clearfix sub-header">
-        <div class="col-sm-5 col-xs-12">
+        <div class="col-sm-1 col-xs-3">
+          <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon.png" alt="" width="100%"></div>
+        </div>
+        <div class="col-sm-5 col-xs-9">
           <h1><?php the_title(); ?></h1>
         </div>
         <div class="col-sm-6 col-xs-12"></div>
@@ -27,11 +30,6 @@ get_header(); ?>
       <!-- Start the Loop. -->
 			<?php if ( have_posts() ) : ?>
 				<?php while ( have_posts() ) : the_post(); ?>
-          <div class="absolute dtl-custom-heading">
-            <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-dtl-icon.png" alt="<?php the_title(); ?>"></div>
-            <h1 style="color: #808080;"><?php the_title(); ?></h1>
-            <hr style="border-top: 3px dotted #808080;">
-          </div>
           <p class="text-right pt-14-100"><?php the_content(); ?></p> 
 				<?php endwhile; ?>
 			<?php endif; ?>                 
