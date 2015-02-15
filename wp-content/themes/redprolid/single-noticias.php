f472f8ad43d0d47bb0ed0fe7bde908b0fe1d9929
Herman Marin <herman.marin@ayuinc.com>
2015-02-15 15:56:51 -0500

    Adding Addthis

--- wp-content/themes/redprolid/single-noticias.php
+++ wp-content/themes/redprolid/single-noticias.php
@@ -3,7 +3,8 @@
     <?php the_breadcrumb(); ?>
     <div class="ph-70 pv-21">
       <div class="row pb-14">
-	      <?php $imagen = get_field('imagen_noticias'); ?>
+	      ss
+	      <?php echo $imagen = get_field('imagen_noticias'); ?>
 				<?php echo $url = $image['url']; ?>
 				<?php echo $title = $image['title']; ?>
 				<?php echo $alt = $image['alt']; ?>
