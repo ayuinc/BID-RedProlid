9b75807f68f94b9f4bfe2838614ba3dfb6c8098c
Herman Marin <herman.marin@ayuinc.com>
2015-02-15 12:25:06 -0500

    Ajuste Sitemap

--- wp-content/themes/redprolid/page-sitemap.php
+++ wp-content/themes/redprolid/page-sitemap.php
@@ -10,22 +10,33 @@ get_header(); ?>
     
     <!--NAV-->
     <?php get_template_part( 'include', 'nav' ); ?>
-    <div class="mh-700">
-      <section id="dtl" class="pt-0">
-        <div class="container relative dtl-content">
-          <!-- Start the Loop. -->
-					<?php if ( have_posts() ) : ?>
-						<?php while ( have_posts() ) : the_post(); ?>
-		          <div class="absolute dtl-custom-heading">
-		            <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-dtl-icon.png" alt="<?php the_title(); ?>"></div>
-		            <h1 style="color: #808080;"><?php the_title(); ?></h1>
-		            <hr style="border-top: 3px dotted #808080;">
-		          </div>
-		          <p class="text-right pt-14-100"><?php the_content(); ?></p> 
-						<?php endwhile; ?>
-					<?php endif; ?>                 
+    
+	<section class="mt-7"> 
+    <div class="container">
+      <?php the_breadcrumb(); ?> 
+      <div class="clearfix sub-header">
+        <div class="col-sm-5 col-xs-12">
+          <h1><?php the_title(); ?></h1>
         </div>
-      </section>      
+        <div class="col-sm-6 col-xs-12"></div>
+      </div>	
     </div>
+	</section>    
+  <section id="dtl" class="pt-0">
+    <div class="container relative dtl-content">
+      <!-- Start the Loop. -->
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+          <div class="absolute dtl-custom-heading">
+            <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-dtl-icon.png" alt="<?php the_title(); ?>"></div>
+            <h1 style="color: #808080;"><?php the_title(); ?></h1>
+            <hr style="border-top: 3px dotted #808080;">
+          </div>
+          <p class="text-right pt-14-100"><?php the_content(); ?></p> 
+				<?php endwhile; ?>
+			<?php endif; ?>                 
+    </div>
+  </section>      
+
 
 <?php get_footer(); ?>
