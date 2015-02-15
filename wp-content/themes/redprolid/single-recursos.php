c04fbc0d8bd7721c82d60064921597149afda349
Herman Marin <herman.marin@ayuinc.com>
2015-02-15 12:30:32 -0500

    Resaltar autor recurso

--- wp-content/themes/redprolid/single-recursos.php
+++ wp-content/themes/redprolid/single-recursos.php
@@ -7,7 +7,7 @@
           <h2 class="medium mb-0"><?php the_title(); ?></h2>
           <?php $publicacion = get_field('recurso_descripcion'); ?>
           <small>
-			      <strong><?php the_field('recurso_autor'); ?></strong>, <?php the_field('recurso_año_de_publicacion'); ?> 
+			      <span class="reslatar"><?php the_field('recurso_autor'); ?></span>, <?php the_field('recurso_año_de_publicacion'); ?> 
   				</small>        
         </div>
       </div>
