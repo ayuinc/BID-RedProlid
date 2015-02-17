a1205a8e9314a123af7d30685b5fb3f47366757e
Gus <yo@soygus.com>
2015-02-17 12:48:15 -0500

    POLL: single

---
+++ wp-content/themes/redprolid/single-poll-main.php
@@ -0,0 +1,21 @@
+<section class="bg-panel"> 
+  <div class="container">
+    <?php the_breadcrumb(); ?>
+    <div class="clearfix sub-header">
+      <div class="col-sm-12 col-xs-12 with-hr pv-0 mv-0">
+        <nav class="text-right text-center-xs">
+          <a href="<?php echo home_url('/'); ?>tu-opinio-cuenta">Tu opinión cuenta</a>
+        </nav>          
+      </div>
+    </div>      
+    <div class="ph-70">
+    	<div class="bg-white border-radius p-21">
+    		<div class="ph-70">
+    			<h1>Qué es lo que más descuidas de tu vida personal cuando estás trabajando?</h1>
+    		</div>
+    		<hr>
+    		<span class="text-gray">Agosto 1 / 2014</span>
+    	</div>
+    </div>
+  </div> 
+</section>
