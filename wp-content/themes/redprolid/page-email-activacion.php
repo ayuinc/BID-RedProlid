5c48e975bfcdfa6f7c880ff9697b492ba6222678
Herman Marin <herman.marin@ayuinc.com>
2015-02-18 07:21:22 -0500

    Activacion email

--- wp-content/themes/redprolid/page-email-activacion.php
+++ wp-content/themes/redprolid/page-email-activacion.php
@@ -32,10 +32,14 @@ get_header(); ?>
     <div class="ph-70 text-center">
     	<h3>Tu email fue activado exitosamente.</h3>
     	<div class="container-sm">
-				<div class="user-sign-in-form pv-21">
-	  			<h2 class="medium">Ingresa con tus claves de accso</h2>
-	        <?php echo do_shortcode('[dm_login_form]'); ?>
-	      </div>
+	    	<div class="row">
+	    		<div class="col-sm-6 col-sm-offset-3">
+						<div class="user-sign-in-form pv-21">
+			  			<h2 class="medium">Ingresa con tus claves de accso</h2>
+			        <?php echo do_shortcode('[dm_login_form]'); ?>
+			      </div>		    		
+		    	</div>
+	    	</div>
     	</div>
     </div>
     <!-- END:IF NO POSTS in FOROS -->
