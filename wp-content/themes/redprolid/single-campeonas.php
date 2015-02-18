8a52d95fd898f9c507f2f9429d1b89609dee79ed
Herman Marin <herman.marin@ayuinc.com>
2015-02-18 07:08:59 -0500

    Noticias imagenes

--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -31,9 +31,11 @@
       <div class="with-hr">
         <p><?php the_field('intro_entrevista_campeona'); ?></p>
         <div class="container-sm">
-	      	<p><strong>País:</strong> <?php the_field('pais_campeona'); ?></p> 
-					<p><strong>Tipo de recurso:</strong> <?php the_field('tipo_de_recurso'); ?></p>
-					<p><strong>Tema:</strong> <?php the_field('tema'); ?></p>
+	      	<p>
+		      	<strong>País:</strong> <?php the_field('pais_campeona'); ?><br>
+						<strong>Tipo de recurso:</strong> <?php the_field('tipo_de_recurso'); ?><br>
+						<strong>Tema:</strong> <?php the_field('tema'); ?>
+					</p>
         </div>
         <div class="content-display">
           <div class="text-right mt-14 mb-14">
