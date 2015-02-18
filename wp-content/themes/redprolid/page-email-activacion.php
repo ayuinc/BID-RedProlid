fdf4dd62d402943efab7f565e3a93e31437e422f
Herman Marin <herman.marin@ayuinc.com>
2015-02-18 07:21:57 -0500

    Activacion email

--- wp-content/themes/redprolid/page-email-activacion.php
+++ wp-content/themes/redprolid/page-email-activacion.php
@@ -14,19 +14,7 @@ get_header(); ?>
   <div class="container">
     <?php the_breadcrumb(); ?>
     <div class="clearfix sub-header">
-      <!-- IF POSTS in FOROS -->
-      <div class="col-sm-1 col-xs-3">
-	      <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/foros_icon.png" alt="" width="100%"></div>
-	    </div>
-      <div class="col-sm-5 col-xs-9">
-        <h1 class="brand-titular">Foro virtual</h1>
-      </div>
-      <div class="col-sm-6 col-xs-12">
-        <nav class="text-right text-center-xs">
-          <a href="<?php echo home_url('/'); ?>charlacafe">Charlacafé >></a>
-        </nav>
-      </div>
-      <!-- END:IF POSTS in FOROS -->
+
     </div> 
     <!-- IF NO POSTS in FOROS -->
     <div class="ph-70 text-center">
