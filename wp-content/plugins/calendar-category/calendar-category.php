d6201dfcc0373457c144ac95c86d0a6547d6810a
Herman Marin <herman.marin@ayuinc.com>
2015-02-19 17:13:11 -0500

    Event Calendar

--- wp-content/plugins/calendar-category/calendar-category.php
+++ wp-content/plugins/calendar-category/calendar-category.php
@@ -266,6 +266,7 @@ function calcat_get_calendar( $initial = true, $echo = true, $category = -1 ) {
 		if ( in_array( $day, $daywithpost ) ) // any posts today?
 				//$calendar_output .= '<a href="' . get_day_link( $thisyear, $thismonth, $day ) . "?cat=$category\" title=\"" . esc_attr( $ak_titles_for_day[$day] ) . "\">$day</a>";
 				//strtolower("Hello WORLD.");
+				echo $ak_titles_for_day[$day];
 				$calendar_output .= '<a href="'. esc_attr( $ak_titles_for_day[$day] ) . "\">$day</a>";
 		else
 			$calendar_output .= $day;
