dc6cfd51418efa374cb7bae3dd966590f9223101
Herman Marin <herman.marin@ayuinc.com>
2015-02-20 08:48:04 -0500

    A tu estilo

--- wp-content/themes/redprolid/index.php
+++ wp-content/themes/redprolid/index.php
@@ -236,9 +236,7 @@ get_header(); ?>
                     <h3 class="pt-0 medium mb-0 pb-0"><?php the_title(); ?></h3>
                     <small><?php $tempDate = get_field(fecha_de_la_entrevista); ?>
             <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?></small>
-                    <p>
-							        <em><?php the_field('posicion_campeona'); ?></em>
-                    </p>
+							      <em><?php the_field('posicion_campeona'); ?></em>
                     <p>
 							        <strong><?php the_field('descripcion_home_campeonas'); ?></strong>
 				            </p>
