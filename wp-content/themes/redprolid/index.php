ba96d170efbc59cba44f3316a479ca2248498fa5
Gus <yo@soygus.com>
2015-02-20 13:07:28 -0500

    HOME: italics

--- wp-content/themes/redprolid/index.php
+++ wp-content/themes/redprolid/index.php
@@ -236,7 +236,7 @@ get_header(); ?>
                     <h3 class="pt-0 medium mb-0 pb-0"><?php the_title(); ?></h3>
                     <small><?php $tempDate = get_field(fecha_de_la_entrevista); ?>
             <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?></small>
-                    <p>
+                    <p class="light-italic">
 							        <em><?php the_field('posicion_campeona'); ?></em>
                     </p>
                     <p>
