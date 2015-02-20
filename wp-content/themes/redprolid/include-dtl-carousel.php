fff2da734f46bd00e02a219633ef6fc39053e349
Gus <yo@soygus.com>
2015-02-20 15:56:42 -0500

    Carousel

--- wp-content/themes/redprolid/include-dtl-carousel.php
+++ wp-content/themes/redprolid/include-dtl-carousel.php
@@ -1,7 +1,7 @@
 <div class="gallery js-flickity dtl-carousel grid-list grid-list-4 grid-list-1-xs">
   <?php query_posts( 'category_name=home-desarrolla-tu-liderazgo&order=ASC' ); ?> 
   <?php while ( have_posts() ) : the_post(); ?>  
-    <div class="gallery-cell dtl-item" data-href="<?php the_field('link_a_seccion_interna_home_dtl'); ?>">
+    <div class="grid-list-item gallery-cell dtl-item" data-href="<?php the_field('link_a_seccion_interna_home_dtl'); ?>">
       <div class="bg-img-block-no-cover bg-img-block" style="background-image: url(<?php the_field('imagen_home_dtl'); ?>);"></div>
       <div class="dtl-title pt-0">
         <h3><?php the_title(); ?></h3>
