66a116b0f2054978beebc4c4a65a0843f90c7b38
Herman Marin <herman.marin@ayuinc.com>
2015-02-21 15:20:03 -0500

    A tu estilo

---
+++ wp-content/themes/redprolid/page-propuesta-exito.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Propuesta Exito
+*/
+
+get_header(); ?>
+
+    <!--HEADER-->
+    <?php get_template_part( 'include', 'header' ); ?>
+    
+    <!--NAV-->
+    <?php get_template_part( 'include', 'nav' ); ?>
+    
+	<section class="mt-7"> 
+    <div class="container">
+      <?php the_breadcrumb(); ?> 
+      <div class="clearfix sub-header">
+        <div class="col-sm-1 col-xs-3">
+          <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon.png" alt="" width="100%"></div>
+        </div>
+        <div class="col-sm-5 col-xs-9">
+          <h1><?php the_title(); ?></h1>
+        </div>
+        <div class="col-sm-6 col-xs-12"></div>
+      </div>	
+    </div>
+	</section>    
+  <section id="dtl" class="pt-0">
+    <div class="container relative dtl-content">
+      <!-- Start the Loop. -->
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+          <p class="text-right pt-14-100"><?php the_content(); ?></p> 
+				<?php endwhile; ?>
+			<?php endif; ?>                 
+    </div>
+  </section>      
+
+
+<?php get_footer(); ?>
