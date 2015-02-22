0540fea56afa81a24a53c436c4012afaca2b5858
Herman Marin <herman.marin@ayuinc.com>
2015-02-22 12:31:22 -0500

    Cambio enterate

--- wp-content/themes/redprolid/single-eventos.php
+++ wp-content/themes/redprolid/single-eventos.php
@@ -13,7 +13,7 @@
 			<div class="banner half-height">
 				<?php if( get_field('imagen_evento') ) { ?>
 				<div class="banner-pic col-sm-3" style="background-image: url(<?php the_field('imagen_evento'); ?>)"></div>
-				<small><?php the_field('redito_imagen_evento'); ?></small>
+				<small><?php the_field('credito_imagen_evento'); ?></small>
 				<?php } else {?>
 				<div class="banner-pic bg-panel col-sm-3"></div>
 				<?php } ?>
