ab0e3c314b8c699b5a2fdbb2df4c5ee15bdf8629
Herman Marin <herman.marin@ayuinc.com>
2015-02-22 10:52:26 -0500

    Cambio enterate

--- wp-content/themes/redprolid/single-recursos.php
+++ wp-content/themes/redprolid/single-recursos.php
@@ -13,7 +13,7 @@
   				<?php if ($folleto_recurso!='') { ?>
   				<div class="pv-14">
   					<img src="<?php echo $folleto_recurso; ?>" width="100%">
-  					<small>Crédito(s): <?php the_field('credito_imagen'); ?></small>   
+  					<small><?php the_field('credito_imagen'); ?></small>   
   				</div>    
   				<?php } ?> 
   				<?php $folleto_adicional_recurso = get_field('folleto_adicional_recurso');?>
