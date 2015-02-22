773ef871ac001ac2586ca5d24ae379e450a922cd
Herman Marin <herman.marin@ayuinc.com>
2015-02-22 15:05:54 -0500

    Cambio enterate

--- wp-content/themes/redprolid/single-video.php
+++ wp-content/themes/redprolid/single-video.php
@@ -11,7 +11,7 @@
     <div class="ph-70">
 	    <h2 class="medium mb-0"><?php the_title(); ?></h2>
 			<?php $tempDate = get_field('fecha_publicacion_video'); ?>
-			<small>Publicado por: <?php the_field('autor_video'); ?> - <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?></small> 	    
+			<small>Publicado por: <?php the_field('autor_video'); ?> el <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?></small> 	    
 	    <iframe width="100%" height="420" src="//www.youtube.com/embed/<?php the_field('video_youtube'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>     
 	    <p class="mt-14"><?php the_field('descripcion_larga_video'); ?></p>			   
 	    <div class="text-center mt-7 mb-7 ">
