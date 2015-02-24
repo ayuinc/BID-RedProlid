9d86d91cfcd7029288ae9d37a132e54d317fd40a
Herman Marin <herman.marin@ayuinc.com>
2015-02-24 18:56:39 -0500

    Cambios

--- wp-content/themes/redprolid/page-a-tu-estilo.php
+++ wp-content/themes/redprolid/page-a-tu-estilo.php
@@ -93,7 +93,7 @@ get_header(); ?>
 							</select>
 						</div>
 				</div>
-				<div class="col-sm-8 bg-secondary p-0-sm pv-7">
+				<div class="col-sm-8 bg-secondary p-0-sm p-7">
 					<nav class="custom-nav">
 						<input type="radio" name="tipo_lugar" value="restaurantes"> Restaurantes
 						<input type="radio" name="tipo_lugar" value="tiendas"> Tiendas
