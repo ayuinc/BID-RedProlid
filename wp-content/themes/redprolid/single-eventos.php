bc90ae973ad6f5c1e61c407246e63d042c1ce8f7
Herman Marin <herman.marin@ayuinc.com>
2015-02-25 12:39:41 -0500

    Cambios

--- wp-content/themes/redprolid/single-eventos.php
+++ wp-content/themes/redprolid/single-eventos.php
@@ -65,12 +65,12 @@
 					    <?php } ?>
 						</div>
 						<div class="col-sm-5">
-							<h5 class="medium mb-0">Fecha de inicio</h5>
+							<h5 class="medium mb-0">Fecha inicio</h5>
 				      <?php $tempDate = get_field('fecha_inicio_evento'); ?>
 				      <p><?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?><?php $lhora_evento = get_field('hora_evento'); ?><?php if ($hora_evento!='') { ?>, <?php the_field('hora_evento'); ?><?php } ?></p>
 				      <?php $tempDateFin = get_field('fecha_fin_evento'); ?>
 				      <?php if ($tempDateFin!='') { ?>
-				      <h5 class="medium mb-0">Fecha de fin</h5>
+				      <h5 class="medium mb-0">Fecha fin</h5>
 				      <p><?php echo date_i18n('j', strtotime( $tempDateFin)); ?> de <?php echo date_i18n('F', strtotime( $tempDateFin)); ?> de <?php echo date_i18n('Y', strtotime( $tempDateFin)); ?></p>
 				      <?php } ?>
 			        <?php $ciudad_evento = get_field('ciudad_evento'); ?>
