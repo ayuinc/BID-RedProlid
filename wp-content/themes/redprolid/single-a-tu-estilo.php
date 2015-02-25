62d5f41c8bc5c71dd325c28b8c39546f6980fa36
Herman Marin <herman.marin@ayuinc.com>
2015-02-25 14:40:07 -0500

    Cambios

--- wp-content/themes/redprolid/single-a-tu-estilo.php
+++ wp-content/themes/redprolid/single-a-tu-estilo.php
@@ -13,14 +13,14 @@
     		<!--<small class="text-gray">Tiendas</small>-->
     		<h1><?php the_title();?></h1>
     		<ul class="list-unstyled pv-21">
-    			<li>Dirección: <?php the_field('estilo_direccion_lugar') ?></li>
+    			<li><strong>Dirección:</strong> <?php the_field('estilo_direccion_lugar') ?></li>
     			<?php $estilo_sitio_web = get_field('estilo_sitio_web'); ?>
     			<?php if ($estilo_sitio_web!='') { ?>
-    			<li>Sitio web: <a href="<?php the_field('estilo_sitio_web'); ?>"><?php the_field('estilo_sitio_web'); ?></a></li>
+    			<li><strong>Sitio web:</strong> <a href="<?php the_field('estilo_sitio_web'); ?>"><?php the_field('estilo_sitio_web'); ?></a></li>
     			<?php } ?>
     			<?php $estilo_fuente_link = get_field('estilo_fuente_link'); ?>
     			<?php if ($estilo_fuente_link!='') { ?>    			
-    			<li>Fuente: <a href="<?php the_field('estilo_fuente_link'); ?>" target="_blank"><?php the_field('estilo_fuente'); ?></a></li>
+    			<li><strong>Fuente:</strong> <a href="<?php the_field('estilo_fuente_link'); ?>" target="_blank"><?php the_field('estilo_fuente'); ?></a></li>
     			<?php } ?>
     		</ul>
     	</div>
