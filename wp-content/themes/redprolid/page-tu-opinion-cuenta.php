158675a05fd4132ced63334523493ede4e678276
carlos calderon <cadermino@gmail.com>
2015-02-27 16:41:10 -0500

    encuesta

--- wp-content/themes/redprolid/page-tu-opinion-cuenta.php
+++ wp-content/themes/redprolid/page-tu-opinion-cuenta.php
@@ -56,9 +56,9 @@ get_header(); ?>
           </div>
           <div class="row">
           	<div class="col-xs-12">
-		          <?php while ( have_posts() ) : the_post(); ?>
-				        <?php get_template_part( 'content', 'page' ); ?>
-				      <?php endwhile; // end of the loop. ?>
+				      <?php while ( have_posts() ) : the_post(); ?>
+		    			  <?php the_content(); ?>
+		    		  <?php endwhile; ?>
 				    </div>
 		      </div>
         </div>
