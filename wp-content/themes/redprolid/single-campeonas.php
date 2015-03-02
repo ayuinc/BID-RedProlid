66b579fc9fad4ab9cc522ff3b7d94f9c0eaa7597
Herman Marin <herman.marin@ayuinc.com>
2015-03-02 12:38:27 -0500

    Cambios

--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -36,11 +36,11 @@
 		        <h2 class="lh-lg mb-0"><?php the_title(); ?></h2>
 		        <small><?php $tempDate = get_field(fecha_de_la_entrevista); ?>
             <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?></small>
-            <p>
-		        		<em>
-		        		<?php the_field('posicion_campeona'); ?>
-								<?php the_field('descripcion_campeona'); ?>
-		        		</em>
+            <p class="mt-14">
+	        		<em>
+	        		<?php the_field('posicion_campeona'); ?>
+							<?php the_field('descripcion_campeona'); ?>
+	        		</em>
             </p>
             
 	        </div>
