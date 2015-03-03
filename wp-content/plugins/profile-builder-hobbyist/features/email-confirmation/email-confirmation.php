7acf9ea6213aa3fe36b27c81d43812b0a1bf9280
Herman Marin <herman.marin@ayuinc.com>
2015-03-02 19:34:16 -0500

    Cambios

--- wp-content/plugins/profile-builder-hobbyist/features/email-confirmation/email-confirmation.php
+++ wp-content/plugins/profile-builder-hobbyist/features/email-confirmation/email-confirmation.php
@@ -349,7 +349,7 @@ function wppb_signup_user_notification( $user, $user_email, $activation_key, $me
 	$subject = sprintf( apply_filters( 'wppb_signup_user_notification_email_subject', __( 'Red PROLID - cuenta activa %2$s', 'profilebuilder'), $user_email, $user, $activation_key, $registration_page_url, $meta, $from_name, 'wppb_user_emailc_registr_w_email_confirm_email_subject' ), $from_name, $user );
 	$message = sprintf( 
 		apply_filters('wppb_signup_user_notification_email_content', 
-			__("Hola,<br><br>Para activar tu cuenta de usuaria o usuario, por favor haz clic en el enlace (final de este mensaje)..\n\nInmediatamente después de hacerlo recibirás un nuevo correo electrónico con tu nombre de usuaria o usuario.<br><br>Si tienes algún problema por favor escríbenos a: comunicate@redprolid.org Muchas gracias,<br><br>Equipo de Red PROLID.<br><br> %s[ACTIVA TU CUENTA]", 
+			__("Hola,<br><br>Para activar tu cuenta de usuaria o usuario, por favor haz clic en el enlace (final de este mensaje)..\n\nInmediatamente después de hacerlo recibirás un nuevo correo electrónico con tu nombre de usuaria o usuario.<br><br>Si tienes algún problema por favor escríbenos a: comunicate@redprolid.org<br><br>Muchas gracias,<br><br>Equipo de Red PROLID.<br><br> %s[ACTIVA TU CUENTA]", 
 				'profilebuilder' ), 
 			$user_email, 
 			$user, 
