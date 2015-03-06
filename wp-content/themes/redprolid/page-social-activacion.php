ef9c2dd1dd07524fa015b9bc065c1d51eaa8e6e1
Herman Marin <herman.marin@ayuinc.com>
2015-03-06 09:33:20 -0500

    Cambios

---
+++ wp-content/themes/redprolid/page-social-activacion.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Social Activacion
+*/
+
+get_header(); ?>
+
+<!--HEADER-->
+<?php get_template_part( 'include', 'header' ); ?>
+
+<!--NAV-->
+<?php get_template_part( 'include', 'nav' ); ?>
+<section> 
+  <div class="container">
+    <!-- IF NO POSTS in FOROS -->
+    <div class="ph-70 text-center">
+    	<div class="container-sm">
+	    	<div class="row pv-49">
+	    		<div class="col-sm-8 col-sm-offset-2">
+		    		<h3 class="medium text-center mt-35">Tu hemos registrado exitosamente.</h3>
+		    		<p>Pronto recibirás un nuevo correo con la confirmación de todos tus datos para poder acceder a Red PROLID.</p>
+	    		</div>
+	    	</div>	    	
+    	</div>
+    </div>
+    <!-- END:IF NO POSTS in FOROS -->
+  </div>
+</section>
+<?php get_footer(); ?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
