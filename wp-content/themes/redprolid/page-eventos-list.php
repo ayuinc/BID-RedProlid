80adf72963b9daef98cf8f115fc8c80fc93ae796
Herman Marin <herman.marin@ayuinc.com>
2015-03-07 15:48:58 -0500

    Cambios

--- wp-content/themes/redprolid/page-eventos-list.php
+++ wp-content/themes/redprolid/page-eventos-list.php
@@ -35,7 +35,7 @@ get_header(); ?>
 					$temp = $wp_query;
 					$wp_query= null;
 					$wp_query = new WP_Query();
-					$wp_query->query('category_name=eventos&offset='.$myOffset.'&posts_per_page=10'.'&paged='.$paged);
+					$wp_query->query('category_name=eventos&offset='.$myOffset.'&showposts=5'.'&paged='.$paged);
 					?>	        
           <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>          
             <div class="title">
