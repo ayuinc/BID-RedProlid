68db8f99f0461a84864050ca432c82819638f32c
Herman Marin <herman.marin@ayuinc.com>
2015-03-07 16:56:51 -0500

    Cambios

--- wp-content/themes/redprolid/single-eventos.php
+++ wp-content/themes/redprolid/single-eventos.php
@@ -103,7 +103,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
+			<div class="row" id="evento_muestra">
 				<div class="col-sm-12">
 					<div class="pv-35">
   		    	<iframe src="<?php the_field('hipervinculo_evento'); ?>" frameborder="0" width="100%" height="700"></iframe>
