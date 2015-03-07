e050b66b9cce05855ac6a231c25fbdcd45c71212
Herman Marin <herman.marin@ayuinc.com>
2015-03-07 16:53:56 -0500

    Cambios

--- wp-content/themes/redprolid/single-eventos.php
+++ wp-content/themes/redprolid/single-eventos.php
@@ -109,7 +109,7 @@
 			<div class="row">
 				<div class="col-sm-12">
 					<div class="pv-35">
-  		    	<iframe src="http://google.com" frameborder="0" width="100%" height="700"></iframe>
+  		    	<iframe src="http://elcomercio.com" frameborder="0" width="100%" height="700"></iframe>
   		    </div>	
 				</div>
 			</div>			
