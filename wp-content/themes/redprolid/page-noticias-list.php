72ed07786643a7845f93c768061f2659abeb4305
Herman Marin <herman.marin@ayuinc.com>
2015-03-07 15:31:53 -0500

    Cambios

--- wp-content/themes/redprolid/page-noticias-list.php
+++ wp-content/themes/redprolid/page-noticias-list.php
@@ -35,7 +35,7 @@ get_header(); ?>
 					$temp = $wp_query;
 					$wp_query= null;
 					$wp_query = new WP_Query();
-					$wp_query->query('category_name=noticias&offset='.$myOffset.'&posts_per_page=10'.'&paged='.$paged);
+					$wp_query->query('category_name=noticias&offset='.$myOffset.'&posts_per_page=5'.'&paged='.$paged);
 					?>	        
           <?php //query_posts( 'category_name=noticias&posts_per_page=10&paged=' . $paged ); ?>
           <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
