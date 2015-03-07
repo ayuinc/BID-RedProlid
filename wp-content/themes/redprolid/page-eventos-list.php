9ecf21b3a091242e53162477dc8af27a0d07664c
Herman Marin <herman.marin@ayuinc.com>
2015-03-07 15:53:17 -0500

    Cambios

--- wp-content/themes/redprolid/page-eventos-list.php
+++ wp-content/themes/redprolid/page-eventos-list.php
@@ -31,12 +31,13 @@ get_header(); ?>
 	        <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
 					<?php
 					global $myOffset;
-					$myOffset = 1;
+					$myOffset = 11;
 					$temp = $wp_query;
 					$wp_query= null;
 					$wp_query = new WP_Query();
-					$wp_query->query('category_name=eventos&offset='.$myOffset.'&showposts=5'.'&paged='.$paged);
+					$wp_query->query('category_name=eventos&offset='.$myOffset.'&posts_per_page=10'.'&paged='.$paged);
 					?>	        
+					<?php echo $paged; ?>
           <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>          
             <div class="title">
               <h3 class="medium text-primary mb-0"><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>         
