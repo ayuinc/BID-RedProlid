4aa66b62f1b8ccb30fc040acacf4c1b475c6b91f
Herman Marin <herman.marin@ayuinc.com>
2015-03-10 13:52:42 -0500

    Cambios

--- wp-content/themes/redprolid/single-noticias.php
+++ wp-content/themes/redprolid/single-noticias.php
@@ -3,15 +3,16 @@
     <?php the_breadcrumb(); ?>
     <div class="ph-70 pv-21">
       <div class="row pb-14">
-  	    <?php if( get_field('imagen_noticias') ) { ?>
-        <div class="col-sm-4">
-          <img src="<?php the_field('imagen_noticias'); ?>" alt="<?php the_field('fuente_imagen_noticias'); ?>" class="img-responsive">
+      	<div class="col-sm-4">	
+      	<?php $imagen = get_field('imagen_noticias'); ?>
+      	<?php if ($imagen!='') { ?>
+      		<img src="<?php the_field('imagen_noticias'); ?>" alt="<?php the_field('fuente_imagen_noticias'); ?>" class="img-responsive">
+      	<?php } else { ?>
+      		<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/02/eventos_redprolid.png" alt="<?php the_field('fuente_imagen_noticias'); ?>" class="img-responsive">
+      	<?php } ?>	         
           <small><?php the_field('fuente_imagen_noticias'); ?></small>
         </div>
-        <div class="col-sm-7">
-  	    <?php } else {?>
-  	    <div class="col-sm-12">  
-        <?php } ?>
+  	    <div class="col-sm-7">  
           <h2 class="medium mb-0"><?php the_title(); ?></h2>
           <?php $publicacion = get_field('publicacion_noticias'); ?>
           <small>
