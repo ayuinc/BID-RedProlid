b1691ded4251f8e74306fd72248bd1e216d73763
Herman Marin <herman.marin@ayuinc.com>
2015-03-10 19:05:39 -0500

    Cambios

--- wp-content/themes/redprolid/page.php
+++ wp-content/themes/redprolid/page.php
@@ -11,14 +11,12 @@ get_header(); ?>
 
 	<section class="pv-70"> 
     <div class="container-sm">
-      <div class="clearfix sub-header">
-        <div class="col-sm-12 col-xs-12">
+
 		      <?php while ( have_posts() ) : the_post(); ?>
 		      	<h1 class="medium ml-14"><?php the_title(); ?></h1>
 		        <?php get_template_part( 'content', 'page' ); ?>
 		      <?php endwhile; // end of the loop. ?>
-        </div>
-      </div>
+
     </div>
 	</section>	
 
