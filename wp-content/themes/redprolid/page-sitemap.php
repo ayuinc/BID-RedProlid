06c7145ba2e53fd331143dc830d5f3de7ddf2eab
Herman Marin <herman.marin@ayuinc.com>
2015-03-11 09:13:55 -0500

    Cambios

--- wp-content/themes/redprolid/page-sitemap.php
+++ wp-content/themes/redprolid/page-sitemap.php
@@ -16,10 +16,10 @@ get_header(); ?>
       <?php the_breadcrumb(); ?> 
       <div class="clearfix sub-header">
         <div class="col-sm-1 col-xs-3">
-          <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon.png" alt="" width="100%"></div>
+          <!--<div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/enterate-icon.png" alt="" width="100%"></div>-->
         </div>
         <div class="col-sm-5 col-xs-9">
-          <h1><?php the_title(); ?></h1>
+          <h1>Mapa del Sitio</h1>
         </div>
         <div class="col-sm-6 col-xs-12"></div>
       </div>	
