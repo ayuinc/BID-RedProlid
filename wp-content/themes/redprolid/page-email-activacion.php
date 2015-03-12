29ac7d5f0a209f034d50097171261c7f7c0476ef
Herman Marin <herman.marin@ayuinc.com>
2015-03-12 15:37:03 -0500

    Cambios

--- wp-content/themes/redprolid/page-email-activacion.php
+++ wp-content/themes/redprolid/page-email-activacion.php
@@ -17,7 +17,7 @@ get_header(); ?>
     	<div class="container-sm">
 	    	<div class="row pv-49">
 	    		<div class="col-sm-8 col-sm-offset-2">
-		    		<h3 class="medium text-center mt-35">Tu cuenta fue activada exitosamente.</h3>
+		    		<h3 class="medium text-center mt-35">Tu correo electrónico fue verificado exitosamente.</h3>
 		    		<p>Pronto recibirás un nuevo correo con la confirmación de todos tus datos para poder acceder a Red PROLID.</p>
 	    		</div>
 	    	</div>	    	
