0e55f1c9a6927b358f9d07a92e963dab74e92f1e
Herman Marin <herman.marin@ayuinc.com>
2015-03-15 18:26:15 -0500

    Cambios

--- wp-content/themes/redprolid/single-video.php
+++ wp-content/themes/redprolid/single-video.php
@@ -20,10 +20,10 @@
 	    	<?php if ($youtube!='') { ?>
 	    		<iframe width="100%" height="420" src="//www.youtube.com/embed/<?php the_field('video_youtube'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>     
 				<?php } ?>
-		    <?php $vimeo = get_field('video_vimeo'); ?>
+		    <?php $vimeo = get_field('video_video_vimeo'); ?>
 	    	<?php if ($vimeo!='') { ?>
 	    	ss				
-					<iframe src="//player.vimeo.com/video/<?php the_field('video_vimeo'); ?>?color=1f3340&title=0&byline=0&portrait=0" width="100%" height="420" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					<iframe src="//player.vimeo.com/video/<?php the_field('video_video_vimeo'); ?>?color=1f3340&title=0&byline=0&portrait=0" width="100%" height="420" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 				<?php } ?>
 				<p><?php the_field('video_descripcion_larga'); ?></p>
 	    </div> 
