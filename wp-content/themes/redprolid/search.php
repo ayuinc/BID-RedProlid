e30095a288e346113b92556b2792d24a93417c7d
Herman Marin <herman.marin@ayuinc.com>
2015-03-16 12:52:17 -0500

    Cambios

--- wp-content/themes/redprolid/search.php
+++ wp-content/themes/redprolid/search.php
@@ -63,7 +63,8 @@ get_header(); ?>
               <li>
                 <h4 class="medium m-0"><?php the_title(); ?></h4>
                 <?php $conte = (wp_get_post_terms(get_the_ID(),'country',array("fields" => "names"))[0]); ?>
-                <?php if (in_category( '263')) { ?>
+                <?php $recursos_autor = get_field('recurso_autor'); ?>
+                <?php if ($recursos_autor!='') { ?>
                 	<small>
                 		<strong>Autor: <?php the_field('recurso_autor'); ?></strong>, <?php the_field('recurso_año_de_publicacion'); ?>
                 	</small>
