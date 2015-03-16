4966b88aa190fed3876543134c636e9f522a20d4
Herman Marin <herman.marin@ayuinc.com>
2015-03-16 08:13:11 -0500

    Cambios

--- wp-content/themes/redprolid/page-video-list.php
+++ wp-content/themes/redprolid/page-video-list.php
@@ -45,7 +45,7 @@ get_header(); ?>
             <?php if ($youtube!='') { ?>
               <iframe width="100%" height="220" src="//www.youtube.com/embed/<?php the_field('video_youtube'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>     
             <?php } ?>
-            <?php $vimeo = get_field('video_vimeo'); ?>
+            <?php $vimeo = get_field('video_video_vimeo'); ?>
             <?php if ($vimeo!='') { ?>        
               <iframe src="//player.vimeo.com/video/<?php the_field('video_video_vimeo'); ?>?color=1f3340&title=0&byline=0&portrait=0" width="100%" height="220" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             <?php } ?>
