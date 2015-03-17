6fb05a2bfc3f996637909181adaf1de408a489e4
Herman Marin <herman.marin@ayuinc.com>
2015-03-17 09:04:02 -0500

    Cambios

--- wp-content/themes/redprolid/search.php
+++ wp-content/themes/redprolid/search.php
@@ -68,8 +68,7 @@ get_header(); ?>
                 	<small>
                 		<?php $recurso_autor = get_field('recurso_autor'); ?>
 										<?php if ($recurso_autor!='') { ?>
-                		<strong>Autor: <?php the_field('recurso_autor'); ?></strong><?php } ?> <?php $recurso_ano_de_publicacion = get_field('recurso_año_de_publicacion'); ?>
-										<?php if ($recurso_ano_de_publicacion!='') { ?>, <?php the_field('recurso_año_de_publicacion'); ?><?php } ?>
+                		<strong>Autor: <?php the_field('recurso_autor'); ?></strong><?php } ?> <?php $recurso_ano_de_publicacion = get_field('recurso_año_de_publicacion'); ?><?php if ($recurso_ano_de_publicacion!='') { ?>, <?php the_field('recurso_año_de_publicacion'); ?><?php } ?>
                 	</small>
                 <?php } else { ?>
                 	<?php $tempDate = get_the_date(); ?>
