4a94e4fcf6d7880b570f28a80e21b13bb45cae70
Herman Marin <herman.marin@ayuinc.com>
2015-03-18 15:06:56 -0500

    Cambios en publicaiones

--- wp-content/themes/redprolid/page-email-activacion-complemento.php
+++ wp-content/themes/redprolid/page-email-activacion-complemento.php
@@ -18,7 +18,7 @@ get_header(); ?>
 	    	<div class="row pv-49">
 	    		<div class="col-sm-8 col-sm-offset-2">
 		    		<h3 class="medium text-center mt-35">Hemos recibido tu información</h3>
-		    		<p>Muy pronto te llegará un correo electrónico de confirmación con el enlace para ingresar a Red PROLID</p>
+		    		<p>Puedes empezar a navegar en Red PROLID</p>
 	    		</div>
 	    	</div>	    	
     	</div>
