d5ef998a02fd55d158a78e0af551d17d7065f4bd
carlos calderon <cadermino@gmail.com>
2015-03-23 18:44:46 -0500

    comments

--- wp-content/themes/redprolid/page-registrate.php
+++ wp-content/themes/redprolid/page-registrate.php
@@ -12,9 +12,9 @@ get_header(); ?>
 <?php //query_posts( 'category_name=puntos-de-vista' ); ?> 
 
 <style>
-	input[type="text"].custom_field_upload{
-		width: 68%;
-		font-size: 0.8rem;
+	.custom_field_upload{
+		width: 68% !important;
+		font-size: 0.8rem !important;
 	}
 </style>
 	<?php if ( have_posts() ) : ?>
