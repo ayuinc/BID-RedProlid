e6b38faea4ec33fb3acb4e4b2f34f4d227a0e11f
carlos calderon <cadermino@gmail.com>
2015-03-23 20:30:45 -0500

    comments

--- wp-content/themes/redprolid/functions.php
+++ wp-content/themes/redprolid/functions.php
@@ -423,12 +423,12 @@ function SearchFilter($query) {
 }
 add_filter('pre_get_posts','SearchFilter');
 
-function wsl_redirect_to( $redirect_to ) {
+function wsl_redirect_to( $redirect_to, $user_id ) {
   //$user = get_user_by( 'email', 'user@example.com' );
   // die(print_r($redirect_to));
   $user = do_action( "wsl_process_login_end_start" );
-  die(print_r($user));
+  die(print_r($user_id));
   return '#comments';
 }
-add_filter( 'wsl_hook_process_login_alter_redirect_to', 'wsl_redirect_to', 10, 1 );
+add_filter( 'wsl_hook_process_login_alter_redirect_to', 'wsl_redirect_to', 10, 2 );
 ?>
