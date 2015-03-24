c74af11d5111092f614ed975d1a7b19836b3e1f9
Herman Marin <herman.marin@ayuinc.com>
2015-03-24 18:33:38 -0500

    Cambios

--- wp-content/themes/redprolid/single-section.php
+++ wp-content/themes/redprolid/single-section.php
@@ -15,7 +15,7 @@ foreach( $customposts as $post ) :  // start you custom loop
 
     <div class="title">
 	    <h3><?php the_title(); ?></h3>
-	    <p>FUENTE: <a href="<?php the_field('url');?>" target="_blank"><?php the_field('blog-debates');?> <?php $debate_tipo_de_url=get_field('debate_tipo_de_url'); ?> <?php if ($debate_tipo_de_url=='video') { ?><img src="http://redprolid.org/wp-content/themes/redprolid/assets/img/enterate-grid-4-01.png" alt="" style="width:20px"><?php } ?></a>, <?php the_field('autor');?></p>
+	    <p>FUENTE: <a href="<?php the_field('url');?>" target="_blank"><?php the_field('blog-debates');?> <?php $debate_tipo_de_url=get_field('debate_tipo_de_url'); ?><?php if ($debate_tipo_de_url=='video') { ?><img src="http://redprolid.org/wp-content/themes/redprolid/assets/img/enterate-grid-4-01.png" alt="" style="width:20px"><?php } ?></a>, <?php the_field('autor');?></p>
     </div>
     
     <?php $youtube = get_field('debate_video_youtube'); ?>
