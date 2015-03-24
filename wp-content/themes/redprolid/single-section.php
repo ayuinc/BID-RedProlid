ac6ddb9626f8f041d6af0f258b65b10f0ad8fa17
Herman Marin <herman.marin@ayuinc.com>
2015-03-24 18:11:36 -0500

    Cambios

--- wp-content/themes/redprolid/single-section.php
+++ wp-content/themes/redprolid/single-section.php
@@ -17,6 +17,26 @@ foreach( $customposts as $post ) :  // start you custom loop
 	    <h3><?php the_title(); ?></h3>
 	    <p>FUENTE: <a href="<?php the_field('url');?>" target="_blank"><?php the_field('blog-debates');?> <img src="http://redprolid.org/wp-content/themes/redprolid/assets/img/enterate-grid-4-01.png" alt="" style="width:20px"></a> <?php the_field('autor');?></p>
     </div>
+    
+    <?php $youtube = get_field('debate_video_youtube'); ?>
+  	<?php if ($youtube!='') { ?>
+    <div class="pv-21">
+    	<iframe width="100%" height="551" src="//www.youtube.com/embed/<?php the_field('debate_video_youtube'); ?>?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>  
+    </div>   
+		<?php } ?>
+    <?php $vimeo = get_field('debate_video_vimeo'); ?>
+  	<?php if ($vimeo!='') { ?>		
+  	<div class="pv-21">	
+			<iframe src="//player.vimeo.com/video/<?php the_field('debate_video_vimeo'); ?>?color=1f3340&title=0&byline=0&portrait=0" width="100%" height="551" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		</div> 		
+		<?php } ?>
+    <?php $vimeo = get_field('debate_imagen'); ?>
+  	<?php if ($vimeo!='') { ?>		
+  	<div class="pv-21">	
+			<img src="<?php the_field('debate_imagen'); ?>" alt="<?php the_title(); ?>" class="img-responsive">
+		</div> 		
+		<?php } ?>		
+        
     <div class="content">
       <?php the_field('contenido-debates'); ?>
     </div>
