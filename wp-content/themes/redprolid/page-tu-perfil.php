593477857b87a824e9b092efac640713946f1259
carlos calderon <cadermino@gmail.com>
2015-04-30 16:12:12 -0500

    tu perfil

--- wp-content/themes/redprolid/page-tu-perfil.php
+++ wp-content/themes/redprolid/page-tu-perfil.php
@@ -59,6 +59,7 @@ get_header(); ?>
           if ( have_posts() ) : while ( have_posts() ) : the_post();
           the_title();
           echo "<br>";
+          echo "<br>";
           endwhile; else:
           echo "Aun no tiene posts";
           endif;
