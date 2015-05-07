79929a2a44216bc9aedf1b270d04273f81f29830
Herman Marin <herman.marin@ayuinc.com>
2015-05-07 15:37:36 -0500

    Cambios en video campeonas

--- wp-content/themes/redprolid/page-enterate.php
+++ wp-content/themes/redprolid/page-enterate.php
@@ -252,7 +252,12 @@ get_header(); ?>
 			                      </div>
 			                    </div>
 			                    <div class="col-sm-6 pl-0-sm pr-0 text-right">
-			                      <img src="<?php the_field('imagen_campeonas'); ?>" alt="" class="img-responsive" style="width: 100%;">
+									          <?php $video = get_field('video_campeonas'); ?>
+									          <?php if ($video!='') { ?>
+									          	<iframe  class="embed-responsive-item" width="100%" height="250" src="https://www.youtube.com/embed/<?php the_field('video_campeonas'); ?>?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+									          <?php } else { ?>
+									          	<img src="<?php the_field('imagen_campeonas'); ?>" alt="" class="img-responsive" style="width: 100%;">
+									          <?php } ?>			                      
 			                    </div>
 					            <?php endwhile; ?>
 										<?php endif; ?> 
