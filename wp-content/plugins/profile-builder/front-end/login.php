6bb3349cde5b68fb5d06e86eb9090b0edb9caa64
Andrea Del Rio <delrio.andre@gmail.com>
2015-05-15 18:28:22 -0500

    backlog

--- wp-content/plugins/profile-builder/front-end/login.php
+++ wp-content/plugins/profile-builder/front-end/login.php
@@ -69,7 +69,7 @@ function wppb_login_redirect( $redirect_to, $redirect_url, $user ){
                 $LostPassURL = apply_filters('wppb_pre_login_url_filter', $LostPassURL);
 
                 if ($user->get_error_code() == 'incorrect_password') {
-                    $error_string = '<strong>' . __('Error', 'profilebuilder') . '</strong>: ' . __('La contraseña que has ingresado para el nombre de usuaria/usuario es incorrecta', 'profilebuilder') . ' ';
+                    $error_string = '<strong>' . __('Error', 'profilebuilder') . '</strong>: ' . __('La contraseña que has ingresado para el correo electrónico es incorrecta', 'profilebuilder') . ' ';
                     $error_string .= '<a href="' . $LostPassURL . '" title="' . __('Password Lost and Found.', 'profilebuilder') . '">' . __('Has perdido tu contraseña', 'profilebuilder') . '</a>?';
 
                     // change the recover password link
