240eb41138524c4bf56eed8d68cd7dafdb486aad
Herman Marin <herman.marin@ayuinc.com>
2015-05-25 09:00:18 -0500

    Ajuste de variable en comentarios

--- wp-content/themes/redprolid/index.php
+++ wp-content/themes/redprolid/index.php
@@ -275,10 +275,6 @@ get_header(); ?>
                         <p class="light" id="home-comment"><a href="<?php echo get_permalink($comment->comment_post_ID); ?>"><?php echo get_the_title( $comment->comment_post_ID ); ?></a></p>
                         <small class="date light"><?php echo($comment->comment_content);?></small></br>
                         <small class="date light"><?php echo($comment->comment_date);?>
-                        <?php 
-	                        $category = get_the_category(); 
-													echo $category[0]->cat_name; 
-												?>
 												<?php //echo($comment->comment_author);?>
 												</small> 
                       </li>
