ccc49007d56edb7eb3ea011a449474a96e4dc3bb
Herman Marin <herman.marin@ayuinc.com>
2015-05-29 08:57:28 -0500

    Cambios css titulos

--- wp-content/themes/redprolid/page-eventos.php
+++ wp-content/themes/redprolid/page-eventos.php
@@ -18,7 +18,7 @@ get_header(); ?>
         <div class="col-sm-1 col-xs-3">
           <div><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/icons/eventos-icon-circle.png" alt="" width="100%"></div>
         </div>
-        <div class="col-sm-5 col-xs-9">
+        <div class="col-sm-5 col-xs-9 pl-14">
           <h1><?php the_title(); ?></h1>
         </div>
         <div class="col-sm-6 col-xs-12">
