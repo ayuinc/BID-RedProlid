a038243a8ba838e73e1244af0c66bb530ee8dafe
Herman Marin <herman.marin@ayuinc.com>
2015-06-18 11:34:01 -0300

    Cambio velocidad slider

--- wp-content/themes/redprolid/index.php
+++ wp-content/themes/redprolid/index.php
@@ -21,7 +21,7 @@ get_header(); ?>
     <div class="mh-700">
       <section id="hero-unit">
         <div class="container pv-21">
-          <div class="gallery js-flickity hidden-sm-down bg-panel border-radius p-21 bg-img-block bg-img-block-top bg-img-block-right" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/red-prolid-network-right.png);" data-flickity-options='{ "cellAlign": "left", "contain": true, "wrapAround": true, "autoPlay": 6000 }'>
+          <div class="gallery js-flickity hidden-sm-down bg-panel border-radius p-21 bg-img-block bg-img-block-top bg-img-block-right" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/red-prolid-network-right.png);" data-flickity-options='{ "cellAlign": "left", "contain": true, "wrapAround": true, "autoPlay": 2000 }'>
             <?php query_posts( 'category_name=video-homepage&posts_per_page=5' ); ?>  
             <?php while ( have_posts() ) : the_post(); ?>
               <div class="grid-list-item gallery-cell clearfix minh-350" style="width: 100%;">
