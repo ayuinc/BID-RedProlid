2f3669f289e94c7d155f2324a6351c1326738301
carlos calderon <cadermino@gmail.com>
2015-06-24 17:09:05 -0500

    video vimeo fix

--- wp-content/themes/redprolid/page-recursos-list.php
+++ wp-content/themes/redprolid/page-recursos-list.php
@@ -64,7 +64,8 @@ get_header(); ?>
 					$wp_query= null;
 					$wp_query = new WP_Query();
 					//$wp_query->query('category_name=recursos&meta_key=recurso_año_de_publicacion&orderby=meta_value_num&offset='.$myOffset.'&showposts=10&paged='.$paged);
-					$wp_query->query('category_name=recursos&meta_key=recurso_año_de_publicacion&orderby=meta_value_num&showposts=10&paged='.$paged);
+					//$wp_query->query('category_name=recursos&meta_key=recurso_año_de_publicacion&orderby=meta_value_num&showposts=10&paged='.$paged);
+					$wp_query->query('category_name=recursos&meta_key=recurso_año_de_publicacion&showposts=10&paged='.$paged);
 					?>	 					       
 	        <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>            
             <div class="title">
