3ebf4246044b9db9c9e48c7d70bea4e210b66266
carlos calderon <cadermino@gmail.com>
2015-07-01 12:42:45 -0500

    linkedin

--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -86,6 +86,14 @@
 	      	<?php $twitter_campeona = get_field('twitter_campeona'); ?>
 					<?php if ($twitter_campeona!='') { ?>
 	      	<strong>Sígueme en Twitter:</strong> <a href="http://twitter.com/<?php the_field('twitter_campeona'); ?>" target="_blank"><?php the_field('twitter_campeona'); ?></a><br>
+	      	<?php } ?>
+	      	<?php $googleplus_campeona = get_field('googleplus_campeona'); ?>
+					<?php if ($googleplus_campeona!='') { ?>
+	      	<strong>Sígueme en Google Plus:</strong> <a href="<?php the_field('googleplus_campeona'); ?>" target="_blank">Google+</a><br>
+	      	<?php } ?>
+	      	<?php $linkedin_campeona = get_field('linkedin_campeona'); ?>
+					<?php if ($linkedin_campeona!='') { ?>
+	      	<strong>Sígueme en LinkedIn:</strong> <a href="<?php the_field('linkedin_campeona'); ?>" target="_blank">LinkedIn</a><br>
 	      	<?php } ?>	      	
 	      	<?php $autor_campeona = get_field('autor_campeona'); ?>
 					<?php if ($autor_campeona!='') { ?>
