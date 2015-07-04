268ded7e5e7c8bfd66a7824f88af12b363fa7496
carlos calderon <cadermino@gmail.com>
2015-07-03 20:47:19 -0500

    problemas con el merge

--- wp-content/themes/redprolid/page-enterate.php
+++ wp-content/themes/redprolid/page-enterate.php
@@ -316,7 +316,7 @@ get_header(); ?>
                             <?php if ($imagen_evento!='') { ?>  
                               <img src="<?php the_field('imagen_evento'); ?>" width="100%">
                             <?php } else { ?>
-                              <img src="/wp-content/uploads/2015/02/eventos_redprolid.png" width="100%">
+                              <!-- <img src="/wp-content/uploads/2015/02/eventos_redprolid.png" width="100%"> -->
                             <?php } ?>
                           </div>
                           <div class="col-md-8">
