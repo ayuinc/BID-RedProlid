627ed40258cd922d6105631d6af6f60aa61b44fb
Herman Marin <herman.marin@ayuinc.com>
2015-07-05 23:43:21 -0500

    Esconder lee mas si no hay hipervinculo

--- wp-content/themes/redprolid/single-noticias.php
+++ wp-content/themes/redprolid/single-noticias.php
@@ -26,7 +26,10 @@
       <div class="row">
         <div class="col-sm-12 with-hr">     
           <p><?php the_field('contenido_noticias'); ?></p>
+          <?php $link_publicacion_noticias = get_field('link_publicacion_noticias'); ?>
+          <?php if ($link_publicacion_noticias!='') { ?>
           <p class="text-right"><a href="<?php the_field('link_publicacion_noticias'); ?>" class="btn btn-primary" target="_blank">Lee la noticia completa</a></p>
+          <?php } ?>
           <div class="text-right">
             <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-sumate-al-debate-footer-icon.png" alt="">
           </div>   
