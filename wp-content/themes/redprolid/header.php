6d0ea7a62bf597421dc6a91e8f60fc00ff03091d
Arabelarc <arabela@laboratoria.la>
2015-07-21 18:02:07 -0500

    arreglo boton menu

--- wp-content/themes/redprolid/header.php
+++ wp-content/themes/redprolid/header.php
@@ -60,8 +60,8 @@
         <div class="left">
           <div class="logo" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/logo_redprolid-header.png)"></div>
         </div>
-        <div class="right pb-14-xs">
-          <button type="button" class="tcon tcon-menu--xcross menu-trigger pb-14-xs pr-14-xs" aria-label="toggle menu">
+        <div class="right pr-14-xs">
+          <button type="button" class="tcon tcon-menu--xcross menu-trigger pb-14-xs " aria-label="toggle menu">
             <span class="tcon-menu__lines" aria-hidden="true"></span>
             <span class="tcon-visuallyhidden">toggle menu</span>
           </button>
