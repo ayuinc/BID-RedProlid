6c87feed5d015087bde1eda13f829bfbaefbfa12
Arabelarc <arabela@laboratoria.la>
2015-09-23 14:59:21 -0500

    arreglo de boton ie11

--- wp-content/themes/redprolid/searchform.php
+++ wp-content/themes/redprolid/searchform.php
@@ -1,7 +1,7 @@
 <form class="form-search-inline" action="<?php echo home_url( '/' ); ?>" method="get">
 	<div class="input-group" style="position: relative !important; right: 214px !important;">
 	  <input type="search" class="form-control" name="s" id="search" value="<?php echo get_search_query(); ?>" placeholder="Busca aquí..." style="width: 572px;position: relative;right: 101px;"/>
-	  <span class="input-group-btn" style="right: 100px;">
+	  <span class="input-group-btn" style="display:block;right: 100px;">
 	    <button type="submit" id="searchsubmit" class="btn btn-primary text-left " style="width: 106px !important ; background-position-x: 58px;"><p class="h3 light text-white mv-0">Buscar</p></button>
 	  </span>
 	</div><!-- /input-group -->
