2811e2cef933bd499f6d57d90c9f4f3597a4eeca
Arabelarc <arabela@laboratoria.la>
2015-11-06 17:57:54 -0500

    cambio de lugar de texto

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -12,9 +12,10 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 		 	<div class="container-md">
 				<div class="row">
 			 		<div class="col-sm-12 col-md-12 col-xs-12">
-			 			<div class="text-description"></div>
+			 			<div class="text-description">
+			 				<p class="text-gray">Somos una inciativa del Banco Interamericano de Desarrollo (BID) para el intercambio de experiencias e ideas entre mujeres que hacen política y que ocupan -- o apiran a ocupar -- posciciones de liderazgo en la administración pública en América Latina.</p>			 				
+			 			</div>
 			 			<div class="video-intro text-center">
-			 				<p class="text-gray">Somos una inciativa del Banco Interamericano de Desarrollo (BID) para el intercambio de experiencias e ideas entre mujeres que hacen política y que ocupan -- o apiran a ocupar -- posciciones de liderazgo en la administración pública en América Latina.</p>
 			 				<iframe width="580" height="315" src="https://www.youtube.com/embed/NOZGTyHWs9M" frameborder="0" allowfullscreen></iframe>
 			 			</div>
 			 		</div>
