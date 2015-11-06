27e42962136e755da7a8c1beb5970b8fcef77763
Arabelarc <arabela@laboratoria.la>
2015-11-06 15:11:53 -0500

    nuevos templates

---
+++ wp-content/themes/redprolid/include-headerprototipo.php
@@ -0,0 +1,28 @@
+<html <?php language_attributes(); ?>>
+  <head>
+
+    <meta charset="utf-8">
+    <!-- Project SEO -->
+    <title><?php wp_title(''); ?></title>
+    
+    <!-- Responsiveness -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="<?php echo content_url('/'); ?>themes/redprolid/assets/img/favicon.ico.png">
+
+    <!-- WebFonts -->
+    <script src="//use.typekit.net/nsv5vjx.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
+
+    <?php wp_head(); ?> <!-- AVISAMOS QUE TERMINA EL HEAD -->
+    <!--[if IE 9]>
+    <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/html5shiv.js"></script>
+    <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/respond.min.js"></script>
+    <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/placeholders.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+<header>
+    
+</header>
--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -1,44 +1,9 @@
 <?php
 /*
-Template Name: new-home
+Template Name: Prototipo Homepage
 */
-
-get_header(); ?>
-
-    <!--HEADER-->
-    <?php get_template_part( 'include', 'header' ); ?>
-    
-    <!--NAV-->
-    <?php get_template_part( 'include', 'nav' ); ?>
-    <section id="dtl">
-      <div class="container dtl-content">
-        <?php the_breadcrumb(); ?> 
-				<?php if ( have_posts() ) : ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-					<div class="col-md-6 col-xs-12 pl-0 pr-0-xs relative">
-	          <div class="absolute dtl-custom-heading pb-0-xs ph-14-xs">
-	            <div class="pl-0 pt-14-xs pr-0-xs" style="width: 91px;"><img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-dtl-icon.png" alt="<?php the_title(); ?>"></div>
-	            <h1 class="mt-7"><?php the_title(); ?></h1>
-	          </div>
-	          <img src="<?php the_field('imagen-dtl'); ?>" alt="<?php the_title(); ?>" class="hidden-xs pull-left" width="100%">
-					</div>  
-					<div class="col-md-6">
-            <h4 class="light"><?php the_field('introduccion-dtl'); ?></h4>
-					</div>
-					<div class="col-md-12 mt-21 pl-0 ph-14-xs">
-						<h4 class="light"><?php the_field('contenido-dtl'); ?></h4>
-					</div>
-					<?php endwhile; ?>
-				<?php endif; ?>                 
-      </div>
-    </section>      
-    <section class="dtl-home pv-14 mv-42-xs">
-      <div class="container ph-70-sm">
-	      <div class="pb-14">
-        <!-- DTL CAROUSEL -->
-        <?php get_template_part( 'include', 'dtl-carousel' ); ?>
-	      </div>
-      </div>
-    </section>
+get_template_part( 'include', 'headerprototipo' ); ?>
+	
+	<div class="site-content minh-490 bg-gray"></div>
 
 <?php get_footer(); ?>
