3452a879c9d88ed6dd9709f2ff211603efad598a
Arabelarc <arabela@laboratoria.la>
2015-11-06 16:05:02 -0500

    prueba de merge

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -13,7 +13,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 		 		<div class="col-sm-12 col-md-12 col-xs-12">
 		 			<div class="text-description"></div>
 		 			<div class="video-intro">
-		 				<iframe width="560" height="315" src="https://www.youtube.com/embed/NOZGTyHWs9M" frameborder="0" allowfullscreen></iframe>
+		 				<iframe width="580" height="315" src="https://www.youtube.com/embed/NOZGTyHWs9M" frameborder="0" allowfullscreen></iframe>
 		 			</div>
 		 		</div>
 		 		<div class="col-sm-12 col-md-12 col-xs-12"></div>	
