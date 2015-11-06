ea8a36abdd4f1a6356b7521fd8622c4877577d57
Arabelarc <arabela@laboratoria.la>
2015-11-06 17:48:44 -0500

    agregando row a section

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -7,16 +7,18 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 	<div class="site-content">
 		<div class="minh-490 bg-img-block" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/women.jpg)">
 			<p class="text-white">Somos una inciativa del Banco Interamericano de Desarrollo (BID) para el intercambio de experiencias e ideas entre mujeres que hacen política y que ocupan -- o apiran a ocupar -- posciciones de liderazgo en la administración pública en América Latina.</p>
-			<div class="search-bar minh-70 bg-pink-redprolid"></div>
+			<div class="search-bar minh-70 bg-pink-redprolid flex"></div>
 		</div>
 		<div class="middle-content">
 		 	<div class="container-md">
-		 		<div class="col-sm-12 col-md-12 col-xs-12">
-		 			<div class="text-description"></div>
-		 			<div class="video-intro text-center">
-		 				<iframe width="580" height="315" src="https://www.youtube.com/embed/NOZGTyHWs9M" frameborder="0" allowfullscreen></iframe>
-		 			</div>
-		 		</div>
+				<div class="row">
+			 		<div class="col-sm-12 col-md-12 col-xs-12">
+			 			<div class="text-description"></div>
+			 			<div class="video-intro text-center">
+			 				<iframe width="580" height="315" src="https://www.youtube.com/embed/NOZGTyHWs9M" frameborder="0" allowfullscreen></iframe>
+			 			</div>
+			 		</div>
+				</div>
 		 		<div class="col-sm-12 col-md-12 col-xs-12"></div>	
 		 		<div class="col-sm-12 col-md-12 col-xs-12"></div>		 			 		
 		 	</div>
