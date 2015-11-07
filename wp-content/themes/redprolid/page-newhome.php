e2bb68ea9dcdd75a084e895a52540f59e4233ba8
Herman Marin <herman.marin@ayuinc.com>
2015-11-07 08:36:45 -0500

    Ingresando formulario Mailchimp

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -7,18 +7,21 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 	<div class="site-content">
 		<div class="minh-490 bg-img-block flex" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/women.jpg)">
 			<div class="registrer-bar minh-70 pv-21 ph-56 bg-pink-redprolid text-center flex-res flex-bar">
+					<form action="//redprolid.us9.list-manage.com/subscribe/post?u=242e37fe564525e1aad9cfc03&amp;id=eb13f7ce93" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					<div class="col-md-3 col-sm-12 col-xs-12 text-right text-center-xs">
 						<p class="bold text-white pt-7">Forma parte de la Red PROLID</p>					
 					</div>
 					<div class="col-md-3 col-sm-12 col-xs-12 col-xs-12 pv-7-xs">
-						<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text">					
+						<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text" id="mce-FNAME" name="FNAME">			
 					</div>
 					<div class="col-md-3 col-sm-12 col-xs-12 col-xs-12 pv-7-xs">
-						<input placeholder="Correo Electrónico" class=" border-radius  col-sm-12 col-xs-12 p-7" type="text">						
+						<input placeholder="Correo Electrónico" name="EMAIL" class=" border-radius col-sm-12 col-xs-12 p-7 required email" type="emaill" id="mce-EMAIL">						
 					</div>
+					<div style="position: absolute; left: -5000px;"><input type="text" name="b_242e37fe564525e1aad9cfc03_eb13f7ce93" tabindex="-1" value=""></div>
 					<div class="col-md-3 col-sm-12 col-xs-12 pv-14-xs text-center-xs text-left">
-						<button class="btn btn-lg btn-outline btn-white border-radius bold">REGÍSTRATE</button>					
+						<input type="submit" value="REGÍSTRATE" name="subscribe" id="mc-embedded-subscribe" class="btn btn-lg btn-outline btn-white border-radius bold">					
 					</div>
+					</form>
 			</div>
 		</div>
 		<section class="middle-content pv-70 bg-gray-lighter">
