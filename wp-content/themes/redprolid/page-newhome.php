c29bfa4f3e4f90093370ba7adcf186753ad7f782
Arabelarc <arabela@laboratoria.la>
2015-11-06 21:09:35 -0500

    adding end content img text anchor

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -76,31 +76,31 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 		<section class="end-content text-center pv-70 bg-gray-lighter">
 			<div class="container-sm">
 				<div class="row">
-					<div class="col-sm-6 col-xs-12 text-left">
+					<div class="col-sm-6 col-xs-12 text-left ph-14">
 						<div class="small-icon text-left">
-							<img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-icon.png" style="widht:3%">							
+							<img class="pv-7" src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-icon.png" style="width:3%">							
 						</div>
 						<div class="img-center-content">
 							<img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-agenda-genero-col.jpg" class="w-100">
 						</div>
 						<div class="col-sm-12 col-xs-12 bg-white">
 							<h5 class="bold text-gray-darker">Carmen Omonte</h5>
-							<small class="italic">2014</small>
+							<small class="italic">2014</small><br>
 							<small class="italic">"Creo que es fundamental que las mujeres creamos, en primer lugar, en nosotras mismas... "</small>
-							<p class="text-gray">Congresista de la República del Perú (2011-2016), y exministra.</p>
+							<p class="text-gray-darker">Congresista de la República del Perú (2011-2016), y exministra.</p>
 							<a class="text-primary">LEE LA ENTREVISTA &#10145;</a>
 						</div>
 					</div>
-					<div class="col-sm-6 col-xs-12 text-left">
+					<div class="col-sm-6 col-xs-12 text-left ph-14">
 						<div class="small-icon text-left">
-							<img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-icon.png" style="widht:3%">							
+							<img class="pv-7" src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-icon.png" style="width:3%">							
 						</div>
 						<div class="img-center-content">
 							<img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/dtl-adonde-vamos-col.jpg" class="w-100">
 						</div>
 						<div class="col-sm-12 col-xs-12 bg-white">
 							<h5 class="bold text-gray-darker">Niki Jhonson</h5>
-							<p class="text-gray">Actualmente se desempeña como docente e invetigadora del instituto de Ciencia y Política (Facultad de Ciencias Sociales, Universidad de la República, Uruguay)</p>
+							<p class="text-gray-darker">Actualmente se desempeña como docente e invetigadora del instituto de Ciencia y Política (Facultad de Ciencias Sociales, Universidad de la República, Uruguay)</p>
 							<a class="text-primary">PARTICIPA EN LA CONVERSQACIÓN &#10145;</a>
 						</div>
 					</div>
