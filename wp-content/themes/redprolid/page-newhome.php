78c65289377c9a27d050439fd27213afd7870171
Arabelarc <arabela@laboratoria.la>
2015-11-06 20:02:44 -0500

    registrer bar

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -6,7 +6,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 	
 	<div class="site-content">
 		<div class="minh-490 bg-img-block flex" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/women.jpg)">
-			<div class="registrer-bar minh-70 p-21 bg-pink-redprolid flex-bar">
+			<div class="registrer-bar minh-70 pv-21 ph-56 bg-pink-redprolid text-center flex-bar">
 					<p class="bold text-white pt-7">Forma parte de la Red PROLID</p>
 					<div class="col-sm-3 col-xs-12">
 						<input placeholder="Nombre" class="border-radius mh-14 col-sm-12 col-xs-12 p-7" type="text">					
