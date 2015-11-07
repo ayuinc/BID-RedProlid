7a46404dd6c2cff9d52543911aa2da6a76d4a147
Herman Marin <herman.marin@ayuinc.com>
2015-11-07 08:49:09 -0500

    Cambios formulario columnas

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -14,13 +14,13 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 									<p class="bold text-white pt-7">Forma parte de la Red PROLID</p>					
 								</div>
 								<div class="col-md-2 col-sm-12 col-xs-12 col-xs-12 pv-7-xs">
-									<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text" id="mce-FNAME" name="FNAME">			
+									<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text" id="mce-FNAME" name="FNAME" style="height:42px">			
 								</div>		
 								<div class="col-md-2 col-sm-12 col-xs-12 col-xs-12 pv-7-xs">
-									<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text" id="mce-LNAME" name="LNAME">			
+									<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text" id="mce-LNAME" name="LNAME" style="height:42px">			
 								</div>										
 								<div class="col-md-2 col-sm-12 col-xs-12 col-xs-12 pv-7-xs">
-									<input placeholder="Correo Electrónico" name="EMAIL" class=" border-radius col-sm-12 col-xs-12 p-7 required email" type="text" id="mce-EMAIL">						
+									<input placeholder="Correo Electrónico" name="EMAIL" class=" border-radius col-sm-12 col-xs-12 p-7 required email" type="text" id="mce-EMAIL" style="height:42px">						
 								</div>
 								<div style="position: absolute; left: -5000px;"><input type="text" name="b_242e37fe564525e1aad9cfc03_eb13f7ce93" tabindex="-1" value=""></div>
 								<div class="col-md-3 col-sm-12 col-xs-12 pv-14-xs text-center-xs text-left">
