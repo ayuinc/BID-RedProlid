5c65c3a0c3b239a3caa445ae82f43d4a4f48688b
Arabelarc <arabela@laboratoria.la>
2015-11-07 15:05:24 -0500

    Agregado de texto header

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -4,10 +4,14 @@ Template Name: Prototipo Homepage
 */
 get_template_part( 'include', 'headerprototipo' ); ?>
 	
-	<div class="site-content">
+	<div class="site-content bg-gray-lighter">
 		<div class="minh-490 bg-img-block flex" style="background-image: url(<?php echo content_url('/'); ?>themes/redprolid/assets/img/women.jpg)">
-			<div class="text-header-first">
-				
+			<div class="text-header-first pt-70-lg mt-70-lg">
+				<small>Únete a la Red</small>
+				<h1 class="mt-0">CONECTAMOS A MUJERES LÍDERES DEL SECTOR PÚBLICO LATINOAMERICANO</h1>
+				<a href="#middle-content">
+					<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACo0lEQVR4Xu2Z8VUUMRDGv6kAO5AOOCoAKtAOhAqwBOlAKwAqECtQKxA7kA6ggvENTvaFIedl93Yz2d3kz7t9b/L98s1kkhBWPmjl+tEANAesnEBLgZUboBXBlgItBVZOoKXAyg3QdoHnFGDmQyL6syY3BM3EzBsA3wHcEdHFGiAw8zWA9wDOBMAPACcq/GbpEFT8uer9KQDeABAIR/rjFRF9WqITmPkzgEvV9hvAaagBFsIFEd0sCQIzy6qL9WX8E0/02PUBCScsBsI28ULiRSOkEO4BvFVSs4dgxD8A2MjKB3e/6gR1V5CacDB3CEb8k9peFrgbyVZ4CRByxL9KgZjMnCHkiv8vAO0QpUmaVTr0Eb8TwNwg9BWfBWALhGMielFMvHsGZj7Vll6mkix4qTlm3weYmiDbyFktEKLzjDR02eKzHRDI1QhhH/G9AWg6yCnqq0JxdYIRL1MSV0rRzh7ZKWC2yLivdoGQED+oax0EQJ3gBmEs8YNSwNsJY4rfG0BpJ4wtfhQApSBMIX40AAkI0iRJRe6OndllOfGhHtN/ATjUvwcVvL0aoRwBphUdBYKKl0tbOZfIGE38qA6ImqV4d9gLwtTiJwGg6SD3iR8UyiAICfFfiOhjjhP7fDO4D9gVhJkHQ0iIvyWicJW9K3Sv/ycDMNQJJcVPlgKmWcp2QmnxRQDkOsFDfDEAKQhEdGycIvt82Oomy3lbICatATaYKYzdO6R5rysmvqgDoj4hrgnh+S1U+KLiXQBoOtwBeGcc8o2I5LKl6CiaApEL7GNs91hZVL19GywZPHqMlbDPL7Ul44dYLg4wToCXeLca4LHS22K6OqAGEA1ADavgOYfmAE/6NcRuDqhhFTzn0BzgSb+G2M0BNayC5xyaAzzp1xD7L9m2aEexDr5GAAAAAElFTkSuQmCC"/>
+				</a>
 			</div>
 			<div class="registrer-bar minh-70 pt-14 ph-56 bg-pink-redprolid text-center flex-res flex-bar">
 					<form action="//redprolid.us9.list-manage.com/subscribe/post?u=242e37fe564525e1aad9cfc03&amp;id=eb13f7ce93" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
@@ -34,7 +38,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 					</form>
 			</div>
 		</div>
-		<section class="middle-content pv-70 bg-gray-lighter">
+		<section id="middle-content" class="middle-content pv-70 bg-gray-lighter">
 		 	<div class="container-sm">
 				<div class="row">
 			 		<div class="col-sm-12 col-md-12 col-xs-12">
@@ -53,7 +57,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 				</div> 			 		
 		 	</div>
 		</section>
-		<section class="center-content text-center pv-70">
+		<section class="center-content text-center bg-white pv-70">
 			<div class="container-sm ph-49">
 	 			<div class="text-description-center-first">
 	 				<h2 class="text-gray-darker bold">¿Qué ganas formando parte de la RED PROLID?</h2>
@@ -91,7 +95,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 		<section class="end-content text-center pv-70 bg-gray-lighter">
 			<div class="container-sm">
 	 			<div class="text-description-center-end">
-	 				<h2 class="text-gray-darker bold">¿Lo último en la Red PROLID?</h2>
+	 				<h2 class="text-gray-darker bold">Lo último en la Red PROLID</h2>
 	 			</div>			
 				<div class="row">
 					<div class="col-sm-6 col-xs-12 text-left ph-14">
@@ -103,7 +107,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 						</div>
 						<div class="col-sm-12 col-xs-12 bg-white ph-21 pt-7 pb-21">
 							<h5 class="bold text-gray-darker mt-7">Carmen Omonte</h5>
-							<small class="italic">2014</small><br>
+							<small class="italic pv-14">2014</small><br>
 							<small class="italic">"Creo que es fundamental que las mujeres creamos, en primer lugar, en nosotras mismas... "</small>
 							<p class="text-gray-darker f-15 pv-7">Congresista de la República del Perú (2011-2016), y exministra.</p>
 							<a class="text-primary f-15 bold">LEE LA ENTREVISTA  →</a>
