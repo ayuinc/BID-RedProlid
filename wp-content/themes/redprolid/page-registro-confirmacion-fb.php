f01b58a5611125e759819500ad1e55dad690c662
Herman Marin <herman.marin@ayuinc.com>
2015-11-11 19:28:52 -0500

    Nueva pagina de gracias

---
+++ wp-content/themes/redprolid/page-registro-confirmacion-fb.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Template Name: Registro Confirmacion FB
+*/
+
+get_header(); ?>
+
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="utf-8">
+    <!-- Project SEO -->
+    <title><?php wp_title(''); ?></title>
+    
+    <!-- Responsiveness -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="<?php echo content_url('/'); ?>themes/redprolid/assets/img/favicon.ico.png">
+
+    <!-- WebFonts -->
+    <script src="//use.typekit.net/nsv5vjx.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
+
+    <?php wp_head(); ?> <!-- AVISAMOS QUE TERMINA EL HEAD -->
+    <!--[if IE 9]>
+    <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/html5shiv.js"></script>
+    <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/respond.min.js"></script>
+    <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/placeholders.min.js"></script>
+    <![endif]-->
+    <!--CRAZZYEGG-->
+		<script type="text/javascript">
+		setTimeout(function(){var a=document.createElement("script");
+		var b=document.getElementsByTagName("script")[0];
+		a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0039/9228.js?"+Math.floor(new Date().getTime()/3600000);
+		a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+		</script>
+  </head>
+  <body>
+   <header class="header-content">
+       <div class="header-logo">
+           <div class="container-sm text-center">
+               <img src="<?php echo content_url('/'); ?>themes/redprolid/assets/img/logo_redprolid-head.png" class="img-responsive" style="width:40%;">
+           </div>
+       </div>
+   </header>
+
+
+<!--NAV-->
+<?php get_template_part( 'include', 'nav' ); ?>
+<section> 
+  <div class="container">
+    <!-- IF NO POSTS in FOROS -->
+    <div class="ph-70 text-center">
+    	<div class="container-sm">
+	    	<div class="row pv-49">
+	    		<div class="col-sm-8 col-sm-offset-2">
+		    		<h3 class="medium text-center mt-35">Hemos recibido tu información</h3>
+		    		<p>Por favor, ingresa tu correo electrónico y contraseña para que puedas empezar a navegar en Red PROLID</p>
+	    		</div>
+	    	</div>	    	
+    	</div>
+    </div>
+    <!-- END:IF NO POSTS in FOROS -->
+  </div>
+</section>
+<?php get_footer(); ?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
