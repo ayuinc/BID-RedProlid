ea3a163b63291df115acb23f4980e450978beaa2
Herman Marin <herman.marin@ayuinc.com>
2015-11-27 14:22:05 -0500

    ajustes

--- wp-content/themes/redprolid/page-registro-confirmacion-fb1.php
+++ wp-content/themes/redprolid/page-registro-confirmacion-fb1.php
@@ -26,21 +26,7 @@ get_header(); ?>
     <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/html5shiv.js"></script>
     <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/respond.min.js"></script>
     <script src="<?php echo content_url('/'); ?>themes/redprolid/js/app/vendor/placeholders.min.js"></script>
-    <![endif]-->
-	  <!-- Facebook Pixel Code -->
-		<script>
-		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-		document,'script','//connect.facebook.net/en_US/fbevents.js');
-		
-		fbq('init', '1005504112842099');
-		fbq('track', "PageView");</script>
-		<noscript><img height="1" width="1" style="display:none"
-		src="https://www.facebook.com/tr?id=1005504112842099&ev=PageView&noscript=1"
-		/></noscript>
-		<!-- End Facebook Pixel Code -->		
+    <![endif]-->	
   </head>
   <body>
    <header class="header-content">
