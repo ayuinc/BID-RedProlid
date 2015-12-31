0817682950cac7faf689b53e6d44aec02ec55773
Herman Marin <herman.marin@ayuinc.com>
2015-12-31 08:49:15 +0700

    Cmabios

--- wp-content/themes/redprolid/searchform.php
+++ wp-content/themes/redprolid/searchform.php
@@ -1,6 +1,6 @@
 <form class="form-search-inline" action="<?php echo home_url( '/' ); ?>" method="get">
-	<div class="input-group" style="position: relative !important; right: 121px !important;">
-	  <input type="search" class="form-control" name="s" id="search" value="<?php echo get_search_query(); ?>" placeholder="Busca aquí..." style="width: 572px;position: relative;right: -2px;"/>
+	<div class="input-group">
+	  <input type="search" class="form-control" name="s" id="search" value="<?php echo get_search_query(); ?>" placeholder="Busca aquí..." style="position: relative;right: -2px;"/>
 	  <span class="input-group-btn" style="display:block;right: -2px;">
 	    <button type="submit" id="searchsubmit" class="btn btn-primary text-left " style="width: 106px !important ; background-position-x: 58px;"><p class="h3 light text-white mv-0">Buscar</p></button>
 	  </span>
