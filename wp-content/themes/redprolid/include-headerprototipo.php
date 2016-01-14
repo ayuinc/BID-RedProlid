801aeb74c58ec95efb71d259e26cbb4880f5214d
Herman Marin <herman.marin@ayuinc.com>
2016-01-14 06:29:31 -0500

    Cambios

--- wp-content/themes/redprolid/include-headerprototipo.php
+++ wp-content/themes/redprolid/include-headerprototipo.php
@@ -72,4 +72,10 @@
         </div>
       </div>
      </div>
+		<script type="text/javascript">
+		setTimeout(function(){var a=document.createElement("script");
+		var b=document.getElementsByTagName("script")[0];
+		a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0044/7636.js?"+Math.floor(new Date().getTime()/3600000);
+		a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+		</script>      
    </header>
