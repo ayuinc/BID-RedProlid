87c76b73aa8247d680d7383f5dcd7368e3677e86
arabelarc <arabelarc@gmail.com>
2016-01-14 16:36:00 -0500

    cambios margenes

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -14,10 +14,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 								<h1 class="h2 text-gray-dark mv-0 pt-21">Alicia del Águila</h1>
 								<h3 class="text-gray-dark medium">Perú: lecciones de un retroceso</h3>
 								<p class="text-gray-dark medium f-15 pb-7">Doctora en Ciencias Sociales por la Universidad Nacional Mayor de San Marcos (Lima), magister en Ciencias Sociales por FLACSO (México) y bachiller en Sociología por Pontificia Universidad Católica del Perú.</p>
-								<a class="text-primary f-12 bold" href="<?php echo home_url('/'); ?>alicia-del-aguila/">LEE EL ARTÍCULO →</a>
-								<!-- 					<a class="mt-35" href="#middle-content">
-									<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACo0lEQVR4Xu2Z8VUUMRDGv6kAO5AOOCoAKtAOhAqwBOlAKwAqECtQKxA7kA6ggvENTvaFIedl93Yz2d3kz7t9b/L98s1kkhBWPmjl+tEANAesnEBLgZUboBXBlgItBVZOoKXAyg3QdoHnFGDmQyL6syY3BM3EzBsA3wHcEdHFGiAw8zWA9wDOBMAPACcq/GbpEFT8uer9KQDeABAIR/rjFRF9WqITmPkzgEvV9hvAaagBFsIFEd0sCQIzy6qL9WX8E0/02PUBCScsBsI28ULiRSOkEO4BvFVSs4dgxD8A2MjKB3e/6gR1V5CacDB3CEb8k9peFrgbyVZ4CRByxL9KgZjMnCHkiv8vAO0QpUmaVTr0Eb8TwNwg9BWfBWALhGMielFMvHsGZj7Vll6mkix4qTlm3weYmiDbyFktEKLzjDR02eKzHRDI1QhhH/G9AWg6yCnqq0JxdYIRL1MSV0rRzh7ZKWC2yLivdoGQED+oax0EQJ3gBmEs8YNSwNsJY4rfG0BpJ4wtfhQApSBMIX40AAkI0iRJRe6OndllOfGhHtN/ATjUvwcVvL0aoRwBphUdBYKKl0tbOZfIGE38qA6ImqV4d9gLwtTiJwGg6SD3iR8UyiAICfFfiOhjjhP7fDO4D9gVhJkHQ0iIvyWicJW9K3Sv/ycDMNQJJcVPlgKmWcp2QmnxRQDkOsFDfDEAKQhEdGycIvt82Oomy3lbICatATaYKYzdO6R5rysmvqgDoj4hrgnh+S1U+KLiXQBoOtwBeGcc8o2I5LKl6CiaApEL7GNs91hZVL19GywZPHqMlbDPL7Ul44dYLg4wToCXeLca4LHS22K6OqAGEA1ADavgOYfmAE/6NcRuDqhhFTzn0BzgSb+G2M0BNayC5xyaAzzp1xD7L9m2aEexDr5GAAAAAElFTkSuQmCC"/>
-								</a> -->					
+								<a class="text-primary f-12 bold" href="<?php echo home_url('/'); ?>alicia-del-aguila/">LEE EL ARTÍCULO →</a>			
 							</div>
 						</div>						
 					</div>
@@ -28,19 +25,16 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 								<h1 class="h2 text-gray-lighter mv-0">Alicia del Águila</h1>
 								<h3>Perú: lecciones de un retroceso</h3>
 								<p class="text-gray-lighter medium f-15 pb-7">Doctora en Ciencias Sociales por la Universidad Nacional Mayor de San Marcos (Lima), magister en Ciencias Sociales por FLACSO (México) y bachiller en Sociología por Pontificia Universidad Católica del Perú.</p>
-								<a class="text-primary f-12 bold" href="<?php echo home_url('/'); ?>alicia-del-aguila/">LEE EL ARTÍCULO →</a>
-								<!-- 					<a class="mt-35" href="#middle-content">
-									<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACo0lEQVR4Xu2Z8VUUMRDGv6kAO5AOOCoAKtAOhAqwBOlAKwAqECtQKxA7kA6ggvENTvaFIedl93Yz2d3kz7t9b/L98s1kkhBWPmjl+tEANAesnEBLgZUboBXBlgItBVZOoKXAyg3QdoHnFGDmQyL6syY3BM3EzBsA3wHcEdHFGiAw8zWA9wDOBMAPACcq/GbpEFT8uer9KQDeABAIR/rjFRF9WqITmPkzgEvV9hvAaagBFsIFEd0sCQIzy6qL9WX8E0/02PUBCScsBsI28ULiRSOkEO4BvFVSs4dgxD8A2MjKB3e/6gR1V5CacDB3CEb8k9peFrgbyVZ4CRByxL9KgZjMnCHkiv8vAO0QpUmaVTr0Eb8TwNwg9BWfBWALhGMielFMvHsGZj7Vll6mkix4qTlm3weYmiDbyFktEKLzjDR02eKzHRDI1QhhH/G9AWg6yCnqq0JxdYIRL1MSV0rRzh7ZKWC2yLivdoGQED+oax0EQJ3gBmEs8YNSwNsJY4rfG0BpJ4wtfhQApSBMIX40AAkI0iRJRe6OndllOfGhHtN/ATjUvwcVvL0aoRwBphUdBYKKl0tbOZfIGE38qA6ImqV4d9gLwtTiJwGg6SD3iR8UyiAICfFfiOhjjhP7fDO4D9gVhJkHQ0iIvyWicJW9K3Sv/ycDMNQJJcVPlgKmWcp2QmnxRQDkOsFDfDEAKQhEdGycIvt82Oomy3lbICatATaYKYzdO6R5rysmvqgDoj4hrgnh+S1U+KLiXQBoOtwBeGcc8o2I5LKl6CiaApEL7GNs91hZVL19GywZPHqMlbDPL7Ul44dYLg4wToCXeLca4LHS22K6OqAGEA1ADavgOYfmAE/6NcRuDqhhFTzn0BzgSb+G2M0BNayC5xyaAzzp1xD7L9m2aEexDr5GAAAAAElFTkSuQmCC"/>
-								</a> -->					
+								<a class="text-primary f-12 bold" href="<?php echo home_url('/'); ?>alicia-del-aguila/">LEE EL ARTÍCULO →</a>				
 							</div>
 					</div>
 				</div>
-				<div class="registrer-bar minh-70 pt-14 ph-56-md bg-pink-redprolid text-center flex-res flex-bar">
+				<div class="registrer-bar minh-70 pt-14 ph-70-md bg-pink-redprolid text-center flex-res flex-bar">
 						<form action="//redprolid.us9.list-manage.com/subscribe/post?u=242e37fe564525e1aad9cfc03&amp;id=eb13f7ce93" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 							<div class="container">
 								<div class="row">
-									<div class="col-md-4 col-sm-12 col-xs-12 pl-0 text-center text-center-xs">
-										<p class="bold text-white text-center pt-7">Forma parte de la Red PROLID</p>					
+									<div class="col-md-4 col-sm-12 col-xs-12 pl-0  text-center-xs">
+										<p class="bold text-white text-left pt-7">Forma parte de la Red PROLID</p>					
 									</div>
 									<div class="col-md-2 col-sm-12 col-xs-12 col-xs-12 pv-7-xs">
 										<input placeholder="Nombre" class="border-radius  col-sm-12 col-xs-12 p-7" type="text" id="mce-FNAME" name="FNAME" style="height:42px">			
@@ -63,18 +57,6 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 		<section id="middle-content" class="middle-content bg-gray-lighter pb-56">
 		 	<div class="container-fluid ml-56-md mr-0">
 				<div class="row">
-					<!-- 			 		<div class="col-sm-12 col-md-12 col-xs-12">
-			 			<div class="icono-content text-center pv-14">
-			 				<img src="<?php //echo content_url('/'); ?>themes/redprolid/assets/img/red-prolid-red-prolid-icon-home.png" class="img-responsive" style="width:6%;">
-			 			</div>
-			 			<div class="text-description">
-			 				<p class="pv-21  ph-56-md mb-0 gray-darker text-center">Somos una iniciativa del Banco Interamericano de Desarrollo (BID)  para el intercambio de experiencias e ideas entre mujeres que hacen política y que ocupan —o aspiran a ocupar— posiciones de liderazgo en la administración pública en América Latina. </p>	
-			 			</div>
-			 			<div class="video-intro text-center">
-			 				<iframe  class="wi-100" width="600" height="315" src="https://www.youtube.com/embed/NOZGTyHWs9M" frameborder="0" allowfullscreen></iframe>
-			 				 <p class="mv-21"><a class="text-primary medium" href="<?php //echo home_url('/'); ?>redprolid/">¿QUIERES SABER MÁS? &#10145;</a></p>
-			 			</div>
-			 		</div> -->
 			 		<div class="col-md-8 col-sm-6 col-xs-12 ph-0-xs bg-white ph-0-xs pv-70-xs pt-70-md hidden-md hidden-lg">
 								<div class="col-md-8 col-sm-12 col-xs-12 text-left ph-14-md">
 									<div class="small-icon text-left pb-14">
@@ -122,19 +104,9 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 			 					</div>
 			 					<div class="border-top-gray-light text-right ph-0 mv-35 col-md-12 col-sm-12">
 			 						<a class="" href="<?php echo home_url('/'); ?>desarrolla-tu-liderazgo/"><small class="text-gray-light pt-7">Todos los debates →</small></a>
-			 					</div>
-			 					
-								<!-- 			 					<div class="col-xs-12 col-sm-6 col-md-4 pl-0 ph-0-xs">
-			 						<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2015/11/instituciones.png">
-			 					</div>
-			 					<div class="col-xs-12 col-sm-6 col-md-6 pl-0-xs">
-			 						<p class="rem-08">XX Congreso Internacional del <br class="hidden-xs"> CLAD</p>
-			 					</div>
-			 					<div class="col-sm-12 col-xs-12 ph-0 pv-7">
-			 						<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>eventos/">LEE TODOS LOS EVENTOS →</a>
-			 					</div> -->
+			 					</div>		 		
 			 			</div>			 			
-				 			<div class="col-md-12 col-sm-6 col-xs-12 text-left ph-0">
+				 			<div class="col-md-10 col-sm-6 col-xs-12 text-left ph-0">
 				 				<div class="small-icon text-left pv-14">
 				 					<h3 class="text-gray-darker color-g inline-p bold">EVENTO</h3>							
 				 				</div>
@@ -152,7 +124,7 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 				 		</div>
 					</div> 			 		
 			 		<div class="col-md-8 col-sm-6 col-xs-12 ph-0-xs bg-white pt-70-md pl-63-md hidden-xs hidden-sm">
-								<div class="col-md-9 col-sm-12 col-xs-12 text-left ph-14-md">
+								<div class="col-md-9 col-sm-12 col-xs-12 text-left pl-14-md pr-0-md">
 									<div class="small-icon text-left pb-14">
 										<h5 class="text-gray color-g inline-p bold">CAMPEON@S</h5>							
 									</div>
@@ -179,74 +151,8 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 									</div>																	
 								</div>
 			 		</div>
-					<!-- 			 		<div class="col-sm-8 col-xs-12 ph-0-xs hidden-lg hidden-md">
-					 		 <div class="col-sm-12 col-xs-12 pl-0 pb-14">
-					 		 	<h5 class="text-gray color-g inline-p bold">EVENTOS</h5>		
-					 		 </div>	
-			 					<div class="col-xs-12 col-sm-6 col-md-4 pl-0 ph-0-xs">
-			 						<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2015/11/cumbre.jpg">
-			 					</div>
-			 					<div class="col-xs-12 col-sm-6 col-md-6 pl-0-xs">
-			 						<p class="rem-08">III Cumbre Iberoamericana de <br class="hidden-xs"> Agendas Locales de Género <br class="hidden-xs"> Mujer y Ciudad-Unión <br class="hidden-xs"> Iberoamericana de <br class="hidden-xs"> Municipalistas (UIM)</p>
-			 					</div>
-			 					<div class="col-xs-12 col-sm-6 col-md-4 pl-0 ph-0-xs">
-			 						<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2015/11/convocatoria.jpg">
-			 					</div>
-			 					<div class="col-xs-12 col-sm-6 col-md-6 pl-0-xs">
-			 						<p class="rem-08">Abierta la convocatoria: <br class="hidden-xs">
-													premios a las mejores prácticas <br class="hidden-xs">
-													locales con enfoque de <br class="hidden-xs">
-													género-Unión Iberoamerciana <br class="hidden-xs">
-													de Municipalistas (UIM)
-									 </p>
-			 					</div>
-			 					<div class="col-xs-12 col-sm-6 col-md-4 pl-0 ph-0-xs">
-			 						<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2015/11/instituciones.png">
-			 					</div>
-			 					<div class="col-xs-12 col-sm-6 col-md-6 pl-0-xs">
-			 						<p class="rem-08">XX Congreso Internacional del <br class="hidden-xs"> CLAD</p>
-			 					</div>
-			 					<div class="col-sm-12 col-xs-12 ph-0 pv-7">
-			 						<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>eventos/">LEE TODOS LOS EVENTOS →</a>
-			 					</div>
-			 		</div> -->
 		 	</div>
-		</section>
-			<!-- 		<section class="end-content text-center pv-70 bg-gray-lighter">
-			<div class="container-sm">
-	 			<div class="text-description-center-end">
-	 				<h2 class="text-gray-darker bold">Lo último en la Red PROLID</h2>
-	 			</div>			
-				<div class="row">
-					<div class="col-sm-6 col-xs-12 text-left ph-14">
-						<div class="small-icon text-left">
-							<img class="pv-7" src="<?php //echo content_url('/'); ?>themes/redprolid/assets/icons/campeonas-icon-circle.png" style="width:13%"> <p class="color-g inline-p bold">Campeon@s</p>							
-						</div>
-						<div class="img-center-content">
-							<img src="<?php //echo content_url('/'); ?>uploads/2015/11/foto-Myrna-Cunningham1.jpg" class="w-100">
-						</div>
-						<div class="col-sm-12 col-xs-12 bg-white ph-21 pt-7 pb-21">
-							<h5 class="bold text-gray-darker mt-7">Mirna Cunningham <small class="italic">2015</small></h5>
-							<p class="text-gray-darker f-15 pv-7">Es la primera vicepresidenta del Consejo Directivo del Fondo para el Desarrollo de los Pueblos Indígenas de América Latina y el Caribe</p>
-							<a class="text-primary f-15 bold" href="<?php //echo home_url('/'); ?>mirna-cunningham/">LEE LA ENTREVISTA  →</a>
-						</div>
-					</div>
-					<div class="col-sm-6 col-xs-12 text-left ph-14">
-						<div class="small-icon text-left">
-							<img class="pv-7" src="<?php //echo content_url('/'); ?>themes/redprolid/assets/img/pdv-main-icon.png" style="width:13%"> <p class="color-g inline-p bold">Puntos de Vista</p>							
-						</div>
-						<div class="img-center-content">
-							<img src="<?php //echo content_url('/'); ?>uploads/2015/11/rocio-silva.jpg" class="w-100">
-						</div>
-						<div class="col-sm-12 col-xs-12 bg-white ph-21 pt-7 pb-21">
-							<h5 class="bold text-gray-darker mt-7">Rocío Silva Santisteban</h5>
-							<p class="text-gray-darker f-15 pv-7">Escritora, profesora universitaria, activista e investigadora en relaciones entre cultura y poder.</p>
-							<a class="text-primary f-15 bold" href="<?php //echo home_url('/'); ?>rocio-silva-santisteban/">PARTICIPA EN LA CONVERSACIÓN  →</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section> --> 	
+		</section> 	
 	</div>
 <?php  get_footer(); ?>
 <script type="text/javascript">
