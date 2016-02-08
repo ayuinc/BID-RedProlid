fec84900cd4dca3025355fbf2079d964ec8f4cb9
Herman Marin <herman.marin@ayuinc.com>
2016-02-08 18:54:39 -0500

    Comentarios

--- wp-content/themes/redprolid/include-encuestassugeridas.php
+++ wp-content/themes/redprolid/include-encuestassugeridas.php
@@ -6,7 +6,7 @@
 			<?php if ($nombretitulo=='Carolina Trivelli') { ?>
 			<div class="col-md-6 ph-0 col-xs-12">
 				<div class="col-md-12 ph-0 col-xs-12 img-entrevistas-sugeridas pb-14">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/yoko.png">
+					<a href="<?php echo home_url('/'); ?>yokasta-guzman/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/yoko.png"></a>
 				</div>
 				<p class="rem-10 bold">Yokasta Guzmán</p>
 				<p class="rem-08 pv-0 medium">"La mujer se ha ido preparando...hay más mujeres con maestrías que hombres, hay más mujeres que están en la universidad que hombres..."</p>
@@ -14,7 +14,7 @@
 			</div>
 			<div class="col-md-6 col-xs-12">
 				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas pb-14">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/isabel.png">
+					<a href="<?php echo home_url('/'); ?>isabel-saint-malo/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/isabel.png"></a>
 				</div>
 				<p class="rem-10 bold">Isabel Saint Malo</p>
 				<p class="rem-08 pv-0 medium">"Lo fundamental de la equidad de género es reconocer que los hombres y las mujeres tenemos distintas cosas que aportar y eso justamente es lo que hace necesario y valioso la participación de las mujeres..."</p>		
@@ -25,7 +25,7 @@
 			<?php if ($nombretitulo=='Isabel Saint Malo') { ?>
 			<div class="col-md-6 ph-0 col-xs-12">
 				<div class="col-md-12 ph-0 col-xs-12 img-entrevistas-sugeridas pb-14">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/yoko.png">
+					<a href="<?php echo home_url('/'); ?>yokasta-guzman/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/yoko.png"></a>
 				</div>
 				<p class="rem-10 bold">Yokasta Guzmán</p>
 				<p class="rem-08 pv-0 medium">"La mujer se ha ido preparando...hay más mujeres con maestrías que hombres, hay más mujeres que están en la universidad que hombres..."</p>
@@ -33,7 +33,7 @@
 			</div>
 			<div class="col-md-6 col-xs-12">
 				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas pb-14">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/carolina.png">
+					<a href="<?php echo home_url('/'); ?>carolina-trivelli-2/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/carolina.png"></a>
 				</div>
 				<p class="rem-10 bold">Carolina Trivelli</p>
 				<p class="rem-08 pv-0 medium">"Lo primero que les diría (a las mujeres)...olvídense de la culpa porque no hay culpa asociada al proceso de querer ser mejor, de querer ser exitosa, de querer llegar más adelante"</p>		
@@ -44,7 +44,7 @@
 			<?php if ($nombretitulo=='Yokasta Guzmán') { ?>
 			<div class="col-md-6 col-xs-12">
 				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas pb-14">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/carolina.png">
+					<a href="<?php echo home_url('/'); ?>carolina-trivelli-2/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/carolina.png"></a>
 				</div>
 				<p class="rem-10 bold">Carolina Trivelli</p>
 				<p class="rem-08 pv-0 medium">"Lo primero que les diría (a las mujeres)...olvídense de la culpa porque no hay culpa asociada al proceso de querer ser mejor, de querer ser exitosa, de querer llegar más adelante"</p>		
@@ -52,7 +52,7 @@
 			</div>		
 			<div class="col-md-6 col-xs-12">
 				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas pb-14">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/isabel.png">
+					<a href="<?php echo home_url('/'); ?>isabel-saint-malo/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/isabel.png"></a>
 				</div>
 				<p class="rem-10 bold">Isabel Saint Malo</p>
 				<p class="rem-08 pv-0 medium">"Lo fundamental de la equidad de género es reconocer que los hombres y las mujeres tenemos distintas cosas que aportar y eso justamente es lo que hace necesario y valioso la participación de las mujeres..."</p>		
