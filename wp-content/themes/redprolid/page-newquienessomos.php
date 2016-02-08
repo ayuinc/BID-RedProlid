6653c966ccc735d8dd9bd5b03d991115e68f0135
arabelarc <arabela>
2016-02-08 15:46:02 -0500

    cambios de imagenes / agregado de imagenes de redes

--- wp-content/themes/redprolid/page-newquienessomos.php
+++ wp-content/themes/redprolid/page-newquienessomos.php
@@ -114,13 +114,13 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 							 	 <div class="collapse-bio">
 							 	 	 <div class="row">
 							 	 	 	<div class="col-xs-12 hidden-md hidden-lg text-left thin">
-							 	 	 		Sígueme en las redes <a href=""></a><a href=""></a>
+							 	 	 		Sígueme en las redes <a href=""><img src="<?php echo content_url('/'); ?>uploads/2016/01/facebook2.png" alt=""></a><a href=""><img src="<?php echo content_url('/'); ?>uploads/2016/01/twitter2.png" alt=""></a>
 							 	 	 	</div>
 							 	 	 	<div class="col-md-6 col-xs-12 text-left">
 							 	 	 		<a id="btnbiografia">Lee su biografía v</a>
 							 	 	 	</div>
 							 	 	 	<div class="col-md-6 hidden-xs hidden-sm text-right thin">
-							 	 	 		Sígueme en las redes <a href=""></a><a href=""></a>
+							 	 	 		Sígueme en las redes <a href=""><img src="<?php echo content_url('/'); ?>uploads/2016/01/facebook2.png" alt=""></a><a href=""><img src="<?php echo content_url('/'); ?>uploads/2016/01/twitter2.png" alt=""></a>
 							 	 	 	</div>
 							 	 	 	<div class="col-md-12 col-xs-12 pv-21">
 							 	 	 		<p class="text-gray thin"> Fue parlamentaria boliviana entre 1997 y 2005 por el Movimiento de la Izquierda Revolucionaria organización con la que se vinculó desde 1974. Fue la cuarta senadora titular en la historia de la República de Bolivia. Presidió, durante cinco años, la Comisión de Descentralización y Desarrollo Local del Senado, y promovió la constitución de las primeras comisiones de Ética Parlamentaria. Impulsó y propuso importantes iniciativas legislativas en los ámbitos municipal, territorial y pro equidad de género, así como reformas constitucionales entre el 2002-2004. Fundadora – hace 32 años- y miembro del Directorio del Centro de Promoción de la Mujer Gregoria Apaza, del Foro Político Nacional de Mujeres y miembro de la Coordinadora de la Mujer. Psicóloga de profesión, con estudios de post grado en género y desarrollo. Magíster en Ciencia Política y cuenta con un diplomado en Educación Superior.</p>
