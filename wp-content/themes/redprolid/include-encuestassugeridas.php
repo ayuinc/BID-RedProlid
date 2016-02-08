8829c402e787d951005c5c35cbd6f2ea1c09887d
Herman Marin <herman.marin@ayuinc.com>
2016-02-08 17:04:30 -0500

    Comentarios

---
+++ wp-content/themes/redprolid/include-encuestassugeridas.php
@@ -0,0 +1,28 @@
+		<div class="row mh-0 pt-35">
+			<h5>Entrevistas Sugeridas</h5>
+			<?php
+			$wp_query = new WP_Query(array( 'cat' => 2 ));
+			$wp_query->query($args);
+			while ($wp_query->have_posts()) : $wp_query->the_post();
+			?>
+			
+			<?php the_field('imagen_campeonas'); ?>
+			
+			<?php endwhile; ?>
+			
+			
+			<div class="col-md-6 ph-0 col-xs-12">
+				<div class="col-md-12 ph-0 col-xs-12 img-entrevistas-sugeridas">
+					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/01/aida-garcia-naranjo.jpg">
+				</div>
+				<p class="rem-08 pv-7 medium">Aída García Naranjo ha sido ministra y embajadora del Perú en la República del Uruguay y representante del Perú ante la Asociación Latinoamericana de Integración (ALADI)</p>
+				<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>carolina-trivelli-2/">LEE MÁS  →</a>
+			</div>
+			<div class="col-md-6 col-xs-12">
+				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas">
+					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2015/11/Lilian-Soto1.jpg">
+				</div>
+				<p class="rem-08 pv-7 medium">Liliana Soto, Presidenta del movimiento socialista y feminista Kuña Pyrenda (Paraguay)</p>		
+				<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>carolina-trivelli-2/">LEE MÁS  →</a>	
+			</div>
+		</div>
--- wp-content/themes/redprolid/single-campeonas.php
+++ wp-content/themes/redprolid/single-campeonas.php
@@ -47,34 +47,7 @@
 			<p><?php the_field('intro_entrevista_campeona'); ?></p>
 			<?php the_field('entrevista_completa_campeona'); ?>
 		</div>
-		<div class="row mh-0 pt-35">
-			<h5>Entrevistas Sugeridas</h5>
-			<?php
-			$wp_query = new WP_Query(array( 'cat' => 2 ));
-			$wp_query->query($args);
-			while ($wp_query->have_posts()) : $wp_query->the_post();
-			?>
-			
-			<?php the_field('imagen_campeonas'); ?>
-			
-			<?php endwhile; ?>
-			
-			
-			<div class="col-md-6 ph-0 col-xs-12">
-				<div class="col-md-12 ph-0 col-xs-12 img-entrevistas-sugeridas">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/01/aida-garcia-naranjo.jpg">
-				</div>
-				<p class="rem-08 pv-7 medium">Aída García Naranjo ha sido ministra y embajadora del Perú en la República del Uruguay y representante del Perú ante la Asociación Latinoamericana de Integración (ALADI)</p>
-				<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>carolina-trivelli-2/">LEE MÁS  →</a>
-			</div>
-			<div class="col-md-6 col-xs-12">
-				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas">
-					<img class="w-100" src="<?php echo content_url('/'); ?>uploads/2015/11/Lilian-Soto1.jpg">
-				</div>
-				<p class="rem-08 pv-7 medium">Liliana Soto, Presidenta del movimiento socialista y feminista Kuña Pyrenda (Paraguay)</p>		
-				<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>carolina-trivelli-2/">LEE MÁS  →</a>	
-			</div>
-		</div>
+		<?php get_template_part( 'include', 'encuestassugeridas' ); ?>
 	  <div class="row">
 			<div class="col-sm-12 pv-21">  
 				<?php	if ( is_user_logged_in() ) { ?>
