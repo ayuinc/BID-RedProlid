911d3c0b3b268c1fb001a0f15b744ba9c3b6bb7b
Herman Marin <herman.marin@ayuinc.com>
2016-02-09 07:35:35 -0500

    Comentarios

--- wp-content/themes/redprolid/page-newhome.php
+++ wp-content/themes/redprolid/page-newhome.php
@@ -18,9 +18,8 @@ get_template_part( 'include', 'headerprototipo' ); ?>
 							</div>
 						</div>						
 					</div>
-					<div class="col-md-6 col-sm-6 img-header flex bg-img-block minh-350 hidden-xs hidden-sm" style="background-image: url(<?php echo content_url('/'); ?>uploads/2016/01/img_2016_02.png)"></div>
-					<div class="col-xs-12 img-header flex bg-img-block minh-420 hidden-md hidden-lg " style="background-image: url(<?php echo content_url('/'); ?>uploads/2016/01/img_2016_02.png)">
-					</div>					
+					<a href="<?php echo home_url('/'); ?>delia-ferreira/"><div class="col-md-6 col-sm-6 img-header flex bg-img-block minh-350 hidden-xs hidden-sm" style="background-image: url(<?php echo content_url('/'); ?>uploads/2016/01/img_2016_02.png)"></div></a>
+					<a href="<?php echo home_url('/'); ?>delia-ferreira/"><div class="col-xs-12 img-header flex bg-img-block minh-420 hidden-md hidden-lg " style="background-image: url(<?php echo content_url('/'); ?>uploads/2016/01/img_2016_02.png)"></div></a>					
 					<div class="col-xs-12 content-header-responsive hidden-md hidden-lg">
 							<div class="container pv-35-xs">
 								<small class="text-gray-darker h6 bold mt-42">PUNTOS DE VISTA</small>
