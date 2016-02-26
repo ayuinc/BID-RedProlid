6c1562cc62abc6b70fa79e55c631ac2ad26f491f
Herman Marin <herman.marin@ayuinc.com>
2016-02-25 22:10:16 -0500

    Cambios

--- wp-content/themes/redprolid/include-encuestassugeridas.php
+++ wp-content/themes/redprolid/include-encuestassugeridas.php
@@ -34,11 +34,11 @@
 			</div>
 			<div class="col-md-6 col-xs-12">
 				<div class="col-md-12 col-xs-12 ph-0 img-entrevistas-sugeridas pb-14">
-					<a href="<?php echo home_url('/'); ?>isabel-saint-malo/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/isabel.png"></a>
+					<a href="<?php echo home_url('/'); ?>otilia-lux/?flag=1"><img class="w-100" src="<?php echo content_url('/'); ?>uploads/2016/02/isabel.png"></a>
 				</div>
-				<p class="rem-10 bold">Isabel Saint Malo</p>
-				<p class="rem-08 pv-0 medium">"Lo fundamental de la equidad de género es reconocer que los hombres y las mujeres tenemos distintas cosas que aportar y eso justamente es lo que hace necesario y valioso la participación de las mujeres..."</p>		
-				<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>isabel-saint-malo/?flag=1">LEE MÁS  →</a>	
+				<p class="rem-10 bold">Otilia Lux de Cotí</p>
+				<p class="rem-08 pv-0 medium">"Yo le diría a las jóvenes que participen en la política...que no tengan miedo,¡participen!"</p>		
+				<a class="text-primary f-15 bold" href="<?php echo home_url('/'); ?>otilia-lux/?flag=1">LEE MÁS  →</a>	
 			</div>
 			<?php } ?>
 			
