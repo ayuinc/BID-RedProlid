f51636e6c5de8b5049154c02a43de2cb43b38f28
Herman Marin <herman.marin@ayuinc.com>
2016-06-20 17:19:49 -0500

    Cambio de Poll

--- wp-content/themes/redprolid/page-puntosdevista-list.php
+++ wp-content/themes/redprolid/page-puntosdevista-list.php
@@ -33,6 +33,7 @@ get_header(); ?>
           $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
           <?php query_posts( 'category_name=puntos-de-vista&posts_per_page=10&offset=1&paged=' . $paged ); ?>
           <?php while ( have_posts() ) : the_post(); ?>
+          kk
             <div class="title">
               <h3 class="medium mb-0"><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php the_title(); ?></a></h3>
               <span><?php the_field('profesion'); ?></span>
