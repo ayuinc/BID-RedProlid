7698a03945814a527965b31aa9bc311f0f249e80
Herman Marin <herman.marin@ayuinc.com>
2016-09-27 11:56:02 +0200

    Ajuste en variable mal escrita

--- wp-content/themes/redprolid/page-eventos.php
+++ wp-content/themes/redprolid/page-eventos.php
@@ -53,7 +53,7 @@ get_header(); ?>
 	        				<?php $tempDate = get_the_date(); ?>
 	              	<p>
 							      <?php $tempDate = get_field('fecha_inicio_evento'); ?>
-							      <strong>Fecha inicio:</strong> <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?><?php $lhora_evento = get_field('hora_evento'); ?><?php if ($hora_evento!='') { ?>, <?php the_field('hora_evento'); ?><?php } ?><br>
+							      <strong>Fecha inicio:</strong> <?php echo date_i18n('j', strtotime( $tempDate)); ?> de <?php echo date_i18n('F', strtotime( $tempDate)); ?> de <?php echo date_i18n('Y', strtotime( $tempDate)); ?><?php $hora_evento = get_field('hora_evento'); ?><?php if ($hora_evento!='') { ?>, <?php the_field('hora_evento'); ?><?php } ?><br>
 							      <?php $tempDateFin = get_field('fecha_fin_evento'); ?>
 							      <?php if ($tempDateFin!='') { ?>
 							      <strong>Fecha fin:</strong> <?php echo date_i18n('j', strtotime( $tempDateFin)); ?> de <?php echo date_i18n('F', strtotime( $tempDateFin)); ?> de <?php echo date_i18n('Y', strtotime( $tempDateFin)); ?><br>
